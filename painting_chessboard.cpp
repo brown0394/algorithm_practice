1652159104b2adbe0a3dab811e12439f724274f9
brown0394 <73463402+brown0394@users.noreply.github.com>
2022-03-06 23:22:29 +0900

    Update painting_chessboard.cpp

--- painting_chessboard.cpp
+++ painting_chessboard.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+char** initializeBoard(int height, int width);
+void deleteBoard(char** board, int height);
+void inputBoardColor(char** board, int height, int width);
+void calculateScore(char** board, char** scoreboard, int height, int width);
 
 int main() {
     int height, width;
@@ -9,6 +13,8 @@ int main() {
     char **scoreboard = initializeBoard(height, width);
 
     inputBoardColor(board, height, width);
+
+    calculateScore(board, scoreboard, height, width);
 
     deleteBoard(board, height);
     deleteBoard(scoreboard, height);
@@ -39,5 +45,30 @@ void inputBoardColor(char** board, int height, int width) {
 }
 
 void calculateScore(char** board, char** scoreboard, int height, int width) {
-    
+    int best = -1;
+    bool white = false;
+
+    for (int i = 0; i < height - 7; ++i) {
+        for (int j = 0; j < width - 7; ++i) {
+            int score = 0;
+            for (int k = i; k < 8; ++k) {
+                for (int l = j; l < 8; ++l) {
+                    if (k == i && j == l) {
+                        if (scoreboard[k][l] == 'b') white = false;
+                        else white = true;
+                    }
+                    if ((scoreboard[k][l] == 'b' && white == true) ||
+                    (scoreboard[k][l] == 'w' && white == false)) {
+                        ++score;
+                    }
+                    if (white) white = false;
+                    else white = true;
+                }
+                if (white) white = false;
+                else white = true;
+            }
+            if (best == -1 || score < best) best = score;
+        }
+    }
+    std::cout << best << std::endl;
 }
