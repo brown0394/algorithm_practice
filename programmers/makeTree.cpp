e132a69f35aea9cb8b9038f6001eac52b33a5b9c
brown0394 <73463402+brown0394@users.noreply.github.com>
2023-09-23 18:26:36 +0900

    Update makeTree.cpp

--- programmers/makeTree.cpp
+++ programmers/makeTree.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
 struct nodeLoc {
@@ -11,43 +12,151 @@ struct nodeLoc {
 
 struct node {
     int num;
+    int x; 
     node* leftChild;
     node* rightChild;
-    node* next;
 };
 
 class tree {
 private:
     node* root;
-    node* createNode(int num);
+    node* createNode(int num, int x);
 public:
-    tree(vector<nodeLoc>& nodes, vector<vector<int>>& nodeinfo);
+    tree(vector<nodeLoc>& nodes);
+    void getPathOrder(vector<vector<int>>& answer);
+    ~tree();
 };
-tree::tree(vector<nodeLoc>& nodes, vector<vector<int>>& nodeinfo) {
-    root = createNode(nodes[0].nodeNum);
-    root->next = root;
-    node* lastlast = nullptr;
-    node* last = root;
-    node* lastCurNode = last;
-    int len = nodeinfo.size();
 
-    int curIdx = 1;
-
-    while (curIdx < len) {
-        if (curIdx > 1 && nodes[curIdx].y < nodes[curIdx - 1].y) {
-            lastlast = last->next;
-            last = 
+tree::~tree() {
+    if (root != nullptr) {
+        stack<pair<node*, short int>> stk;
+        node* cur = root;
+        if (root->leftChild != nullptr) {
+            stk.push(make_pair(root, 1));
+            cur = root->leftChild;
+        }
+        else if (root->rightChild != nullptr) {
+            stk.push(make_pair(root, 2));
+            cur = root->rightChild;
+        }
+        else {
+            delete root;
+            return;
         }
         
+        short int nextStep = 0;
+        while (true) {
+            if (nextStep < 2) {
+                if (nextStep == 0 && cur->leftChild != nullptr) {
+                    stk.push(make_pair(cur, 1));
+                    nextStep = 0;
+                    cur = cur->leftChild;
+                    continue;
+                }
+                if (cur->rightChild != nullptr) {
+                    stk.push(make_pair(cur, 2));
+                    nextStep = 0;
+                    cur = cur->rightChild;
+                    continue;
+                }
+            }
+            if (cur == root) break;
+            delete cur;
+            cur = stk.top().first;
+            nextStep = stk.top().second;
+            stk.pop();
+        }
+        delete root;
     }
 }
 
-node* tree::createNode(int num) {
+tree::tree(vector<nodeLoc>& nodes) {
+    root = createNode(nodes[0].nodeNum, nodes[0].x);
+    node* cur;
+
+    for (auto it = nodes.begin()+1; it != nodes.end(); ++it) {
+        cur = root;
+        while (true) {
+            if (it->x < cur->x) {
+                if (cur->leftChild != nullptr) {
+                    cur = cur->leftChild;
+                }
+                else {
+                    cur->leftChild = createNode(it->nodeNum, it->x);
+                    break;
+                }
+            }
+            else {
+                if (cur->rightChild != nullptr) {
+                    cur = cur->rightChild;
+                }
+                else {
+                    cur->rightChild = createNode(it->nodeNum, it->x);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void tree::getPathOrder(vector<vector<int>>& answer) {
+    if (root != nullptr) {
+        stack<pair<node*, short int>> stk;
+        node* cur = root;
+        if (root->leftChild != nullptr) {
+            stk.push(make_pair(root, 1));
+            cur = root->leftChild;
+        }
+        else if (root->rightChild != nullptr) {
+            stk.push(make_pair(root, 2));
+            cur = root->rightChild;
+        }
+        else {
+            answer[0].push_back(root->num);
+            answer[1].push_back(root->num);
+            return;
+        }
+        answer[0].push_back(root->num);
+        short int nextStep = 0;
+        while (true) {
+            if (nextStep < 2) {
+                if (nextStep == 0 && cur->leftChild != nullptr) {
+                    stk.push(make_pair(cur, 1));
+                    answer[0].push_back(cur->num);
+                    nextStep = 0;
+                    cur = cur->leftChild;
+                    continue;
+                }
+                if (cur->rightChild != nullptr) {
+                    stk.push(make_pair(cur, 2));
+                    if (!nextStep) {
+                        answer[0].push_back(cur->num);
+                    }
+                    nextStep = 0;
+                    cur = cur->rightChild;
+                    continue;
+                }
+            }
+            if (cur == root) break;
+            if (!nextStep) {
+                answer[0].push_back(cur->num);
+            }
+            answer[1].push_back(cur->num);
+            cur = stk.top().first;
+            nextStep = stk.top().second;
+            stk.pop();
+        }
+        answer[1].push_back(cur->num);
+    }
+}
+
+node* tree::createNode(int num, int x) {
     node* newNode = new node;
     newNode->num = num;
     newNode->leftChild = nullptr;
     newNode->rightChild = nullptr;
-    newNode->next = nullptr;
+    newNode->x = x;
+    return newNode;
 }
 
 bool sortStandard(nodeLoc& one, nodeLoc& two) {
@@ -64,14 +173,25 @@ void print(vector<nodeLoc>& nodes) {
 }
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
-    vector<vector<int>> answer;
+    vector<vector<int>> answer(2);
     vector<nodeLoc> nodes;
     nodes.reserve(nodeinfo.size());
     for (int i = 0; i < nodeinfo.size(); ++i) {
         nodes.push_back({ i + 1, nodeinfo[i][0], nodeinfo[i][1] });
     }
     sort(nodes.begin(), nodes.end(), sortStandard);
-
-
+    tree pathTree(nodes);
+    pathTree.getPathOrder(answer);
     return answer;
+}
+
+int main() {
+    vector<vector<int>> nodeinfo {{5, 3}, { 2, 1 }};
+    vector<vector<int>> answer = solution(nodeinfo);
+    for (auto it = answer.begin(); it != answer.end(); ++it) {
+        for (auto it2 = it->begin(); it2 != it->end(); ++it2) {
+            printf("%d ", *it2);
+        }
+        putchar('\n');
+    }
 }
