cc0485f028a56fc29bfafbcfc3edc8c6edc8b090
brown0394 <73463402+brown0394@users.noreply.github.com>
2023-10-07 00:22:17 +0900

    Update z.cpp

--- z.cpp
+++ z.cpp
@@ -3,13 +3,13 @@
 int main() {
 	int row, col, n;
 	scanf_s("%d %d %d", &n, &row, &col);
-	int table = 2 << n;
+	int table = 2 << (n - 1);
 	int count = 0;
 	int half = table;
 	int  lim = table;
 	int rowBase = 0;
 	int colBase = 0;
-	while (true) {
+	while (lim > 1) {
 		lim = half;
 		half = half >> 1;
 		if (row < (rowBase + half) && col < (colBase + half)) {
@@ -20,7 +20,7 @@ int main() {
 			colBase += half;
 			continue;
 		}
-		if (row < (rowBase + lim) && (colBase + half)) {
+		if (row < (rowBase + lim) && col < (colBase + half)) {
 			count += half * half * 2;
 			rowBase += half;
 			continue;
@@ -33,4 +33,6 @@ int main() {
 		}
 
 	}
+
+	printf("%d\n", count);
 }
