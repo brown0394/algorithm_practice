6cfdcbde76a51600ae15af60cd6be6ed476bf5ea
brown0394 <73463402+brown0394@users.noreply.github.com>
2023-11-28 23:07:35 +0900

    Create hidingPhoneNumber.cpp

---
+++ programmers/hidingPhoneNumber.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer;
+    int i = 0;
+    for (i = 0; i < phone_number.size()-4; ++i) {
+        answer.push_back('*');
+    }
+    for (; i < phone_number.size(); ++i) {
+        answer.push_back(phone_number[i]);
+    }
+    return answer;
+}
