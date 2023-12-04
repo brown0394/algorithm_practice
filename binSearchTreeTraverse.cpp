7897aa0a5766fb01cf0efea1de64dca451442e90
brown0394 <73463402+brown0394@users.noreply.github.com>
2023-12-05 00:33:16 +0900

    Update binSearchTreeTraverse.cpp

--- binSearchTreeTraverse.cpp
+++ binSearchTreeTraverse.cpp
@@ -3,21 +3,37 @@
 int main() {
 	int in;
 	int stk[10000];
-	int idx = 0;
+	int idx = 1;
+	scanf_s("%d", &stk[0]);
 	while (std::cin >> in) {//scanf_s("%d", &in) != EOF) {
-		if (idx) {
-			if (in > stk[idx - 1]) {
-				--idx;
-				while (idx) {
-					if (stk[idx - 1] < in) {
+		if (in > stk[idx - 1]) {
+			--idx;
+			while (idx) {
+				if (stk[idx - 1] < in) {
+					if (stk[idx - 1] > stk[idx]) {
 						printf("%d\n", stk[idx--]);
 					}
 					else {
+						int i = idx - 1;
+						while (i) {
+							if (stk[i] < stk[i - 1]) {
+								if (in > stk[i - 1]) {
+									for (; idx >= i; --idx) {
+										printf("%d\n", stk[idx]);
+									}
+								}
+								break;
+							}
+							--i;
+						}
 						break;
 					}
 				}
-				++idx;
+				else {
+					break;
+				}
 			}
+			++idx;
 		}
 		stk[idx++] = in;
 	}
