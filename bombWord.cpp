5195b889f9707b43e2a8868001462c6967a59b65
brown0394 <73463402+brown0394@users.noreply.github.com>
2023-12-30 00:30:58 +0900

    Update bombWord.cpp

--- bombWord.cpp
+++ bombWord.cpp
@@ -1,57 +1,66 @@
 #include <iostream>
 #include <cstring>
-#include <stack>
+#include <vector>
 
-
-struct node {
-	int start;
-	int end;
-};
-
-int main() {
+class StringBomb {
+private:
 	char str[1000001];
 	char bomb[37];
-	scanf_s("%s %s", str, 1000001, bomb, 37);
-	std::stack<node> stk;
-	int bombLen = strlen(bomb);
-	int strLen = strlen(str);
-	int newIdx = 0;
-	for (int i = 0; i < strLen; ++i) {
-		if (str[i] == bomb[0] && i + bombLen <= strLen) {
-			int idx = 0;
-			for (; idx < bombLen; ++idx) {
-				if (str[i + idx] != bomb[idx]) break;
-				else str[newIdx++] = str[i + idx];
-			}///
-			if (idx == bombLen) {
-				newIdx -= bombLen;
-				while (!stk.empty() && stk.top().end == newIdx) {
-					int base = stk.top().end - stk.top().start;
-					stk.pop();
-					if (i + (bombLen - base) <= strLen) {
-						int j = base;
-						for (; j < bombLen; ++j) {
-							if (str[i] != bomb[j]) break;
-							else str[newIdx++] = str[i];
-							++i;
-						}
-						if (j == bombLen) newIdx -= bombLen;
-					}
+	int bombLen;
+	int strLen;
+	int checkBomb(int i);
+public:
+	StringBomb();
+	void removeBomb();
+	void print();
+};
+
+StringBomb::StringBomb() {
+	scanf_s("%s %s", &str, 1000001, &bomb, 37);
+	bombLen = strlen(bomb);
+	strLen = strlen(str);
+}
+
+int StringBomb::checkBomb(int i) {
+	std::vector<int> trace;
+	if (i + bombLen <= strLen) {
+		for (int j = 0; j < bombLen; ++j) {
+			if (str[i + j] != bomb[j]) {
+				for (int k = 0; k < trace.size(); ++k) {
+					str[trace[k]] = bomb[k];
 				}
-				--i;
+				return i;
 			}
-			else {
-				stk.push({ newIdx - idx, newIdx });
-				--i;
+			trace.push_back(i + j);
+			if (j && str[i + j] == bomb[0]) {
+				int val = checkBomb(i + j);
+				if (val != i + j) {
+					if (str[i + j] == '-') {
+						--j;
+						trace.pop_back();
+					}
+					i = val - j;
+				}
 			}
+			str[i + j] = '-';
 		}
-		else {
-			str[newIdx++] = str[i];
+		return i + bombLen - 1;
+	}
+	return i;
+}
+
+void StringBomb::removeBomb() {
+	for (int i = 0; i < strLen; ++i) {
+		if (str[i] == bomb[0]) {
+			int val = checkBomb(i);
+			if (val == i) printf("%c", str[i]);
+			else i = val;
 		}
+		else if (str[i] != '-') printf("%c", str[i]);
 	}
-	if (newIdx) {
-		str[newIdx] = '\0';
-		printf("%s\n", str);
-	}
-	else printf("FRULA\n");
+}
+
+int main() {
+	StringBomb str;
+	str.removeBomb();
 }
