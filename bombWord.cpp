608bb2fced966e321ac0a7a4fa0019c273a7f7b5
brown0394 <73463402+brown0394@users.noreply.github.com>
2023-12-30 23:45:00 +0900

    Update bombWord.cpp

--- bombWord.cpp
+++ bombWord.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <vector>
 
 class StringBomb {
 private:
-	char str[1000001];
-	char bomb[37];
+	std::string str;
+	std::string bomb;
 	int bombLen;
 	int strLen;
 	int checkBomb(int i);
@@ -16,32 +16,36 @@ public:
 };
 
 StringBomb::StringBomb() {
-	scanf_s("%s %s", &str, 1000001, &bomb, 37);
-	bombLen = strlen(bomb);
-	strLen = strlen(str);
+	std::cin >> str >> bomb;
+	bombLen = bomb.size();
+	strLen = str.size();
 }
 
 int StringBomb::checkBomb(int i) {
+	int init = i;
 	std::vector<int> trace;
 	if (i + bombLen <= strLen) {
 		for (int j = 0; j < bombLen; ++j) {
+			if (str[i + j] == '-') {
+				++i;
+				--j;
+				continue;
+			}
+			if (j && str[i + j] == bomb[0]) {
+				int val = checkBomb(i + j);
+				if (val != i + j) {
+					--j;
+					i = val - j;
+					continue;
+				}
+			}
 			if (str[i + j] != bomb[j]) {
 				for (int k = 0; k < trace.size(); ++k) {
 					str[trace[k]] = bomb[k];
 				}
-				return i;
+				return init;
 			}
 			trace.push_back(i + j);
-			if (j && str[i + j] == bomb[0]) {
-				int val = checkBomb(i + j);
-				if (val != i + j) {
-					if (str[i + j] == '-') {
-						--j;
-						trace.pop_back();
-					}
-					i = val - j;
-				}
-			}
 			str[i + j] = '-';
 		}
 		return i + bombLen - 1;
@@ -50,14 +54,22 @@ int StringBomb::checkBomb(int i) {
 }
 
 void StringBomb::removeBomb() {
+	bool frula = true;
 	for (int i = 0; i < strLen; ++i) {
 		if (str[i] == bomb[0]) {
 			int val = checkBomb(i);
-			if (val == i) printf("%c", str[i]);
+			if (val == i) {
+				printf("%c", str[i]);
+				frula = false;
+			}
 			else i = val;
 		}
-		else if (str[i] != '-') printf("%c", str[i]);
+		else if (str[i] != '-') {
+			printf("%c", str[i]);
+			frula = false;
+		}
 	}
+	if (frula) printf("FRULA\n");
 }
 
 int main() {
