4cad995095da7cc57f1149cb89c21ae930ca653c
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-01-18 01:41:02 +0900

    Update josephusProb.cpp

--- josephusProb.cpp
+++ josephusProb.cpp
@@ -1,78 +1,23 @@
 #include <iostream>
-#include <vector>
-
-struct node {
-	int num;
-	node* next;
-	node* prev;
-};
-
-class circularList {
-private:
-	node* head;
-	int count;
-public:
-	circularList() : head(nullptr) { count = 0; }
-	void addNode(int num);
-	void removeJosephus(int interval);
-	~circularList();
-};
-
-void circularList::addNode(int num) {
-	++count;
-	if (head == nullptr) {
-		head = new node;
-		head->next = head;
-		head->prev = head;
-		head->num = num;
-	}
-	else {
-		node* newOne = new node{ num, head, head->prev };
-		head->prev->next = newOne;
-		head->prev = newOne;
-	}
-}
-
-
-void circularList::removeJosephus(int interval) {
-	node* cur = head->prev;
-	node* temp;
-	bool first = true;
-	while (count) {
-		for (int i = 0; i < interval; ++i) {
-			cur = cur->next;
-		}
-		if (first) {
-			first = false;
-			printf("<%d", cur->num);
-		}
-		else printf(", %d", cur->num);
-		temp = cur;
-		cur->prev->next = cur->next;
-		cur->next->prev = cur->prev;
-		cur = cur->prev;
-		delete  temp;
-		--count;
-	}
-	printf(">\n");
-}
-
-circularList::~circularList() {
-	node* next;
-	for (int i = 0; i < count; ++i) {
-		next = head->next;
-		delete head;
-		head = next;
-	}
-}
+#include <string>
 
 int main() {
 	int num, gap;
 	scanf_s("%d %d", &num, &gap);
-	circularList josephus;
-	for (int i = 1; i <= num; ++i) {
-		josephus.addNode(i);
+	int josephus[5002];
+	for (int i = 0; i < num; ++i) {
+		josephus[i] = i + 1;
 	}
-	josephus.removeJosephus(gap);
-
+	--gap;
+	int idx = gap % num;
+	printf("<%d", josephus[idx]);
+	memmove(&josephus[idx], &josephus[idx + 1], (num - 1 - idx) * 4);
+	--num;
+	while (num) {
+		idx = (idx + gap) % num;
+		printf(", %d", josephus[idx]);
+		memmove(&josephus[idx], &josephus[idx + 1], (num - 1 - idx) * 4);
+		--num;
+	}
+	printf(">\n");
 }
