9abaff121ae5ca70b324476cd6c1dd6384091b9c
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-02-14 23:56:48 +0900

    Update zigzagQueue.cpp

--- zigzagQueue.cpp
+++ zigzagQueue.cpp
@@ -2,12 +2,61 @@
 #include <vector>
 #include <bitset>
 
-std::bitset<101> visited;
-int posCount;
-int size;
+struct rec {
+	int smallerCase;
+	int biggerCase;
+	rec() {
+		smallerCase = -1;
+		biggerCase = -1;
+	}
+};
 
+struct node {
+	int num;
+	node* prev;
+	node* next;
+};
 
-void dfs(int depth, int num, bool bigger) {
+class circList {
+private:
+	node* head;
+	node* last;
+public:
+	circList(int size);
+};
+
+circList::circList(int size) {
+	for (int i = 1; i <= size; ++i) {
+
+	}
+}
+
+class zigZag {
+private:
+	std::bitset<101> visited;
+	std::vector<std::vector<std::vector<rec>>> record;
+	std::vector<std::pair<int, int>> biggerSmaller;
+	int posCount;
+	int size;
+	void dfs(int depth, int num, bool bigger);
+	void countPossiblity();
+public:
+	zigZag(int p_size);
+};
+
+zigZag::zigZag(int p_size) : size(p_size),
+record(size + 1, std::vector<std::vector<rec>>(size, std::vector<rec>(size))),
+biggerSmaller(size + 1) {
+	int half = size >> 1;
+	if (size % 2) ++half;
+	for (int i = 1; i <= size; ++i) {
+		biggerSmaller[i].first = size - i;
+		biggerSmaller[i].second = size - biggerSmaller[i].first - 1;
+	}
+	int posCount = 0;
+}
+
+void zigZag::dfs(int depth, int num, bool bigger) {
 	if (depth == size) {
 		posCount = (posCount + 1) % 1000000;
 		return;
@@ -33,8 +82,7 @@ void dfs(int depth, int num, bool bigger) {
 	}
 }
 
-void countPossiblity() {
-	posCount = 0;
+void zigZag::countPossiblity() {
 	if (size == 1) {
 		posCount = 1;
 		return;
@@ -48,7 +96,7 @@ void countPossiblity() {
 }
 
 int main() {
-	scanf("%d", &size);
-	countPossiblity();
-	printf("%d\n", posCount);
+	int size = 0;
+	scanf_s("%d", &size);
+
 }
