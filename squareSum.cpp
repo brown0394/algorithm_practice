7055926d1f0dbe42b1b025a44c9502a8b6d9be2e
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-02-20 00:04:54 +0900

    Update squareSum.cpp

--- squareSum.cpp
+++ squareSum.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+
+
 int main() {
 	int num;
 	std::vector<int> rec(100001, 100001);
@@ -8,18 +10,13 @@ int main() {
 	rec[2] = 2;
 	rec[3] = 3;
 	scanf_s("%d", &num);
-
 	for (int i = 1; i <= 316; ++i) {
+		if (i * i > num) break;
 		rec[i * i] = 1;
 	}
-
-	for (int i = 4; i <= num; ++i) {
-		int low = 1;
-		int high = i - 1;
-		while (low <= high) {
-			if (rec[i] > rec[low] + rec[high]) rec[i] = rec[low] + rec[high];
-			++low;
-			--high;
+	for (int i = 5; i <= num; ++i) {
+		for (int j = 1; j * j <= i; ++j) {
+			if (rec[i - (j * j)] + 1 < rec[i]) rec[i] = rec[i - (j * j)] + 1;
 		}
 	}
 	
