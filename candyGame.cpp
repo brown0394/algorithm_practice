4aca2ea2ad655896ab2a6179d7e8361ea4dcc162
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-02-24 22:25:05 +0900

    Update candyGame.cpp

--- candyGame.cpp
+++ candyGame.cpp
@@ -1,62 +1,75 @@
 #include <iostream>
 
+char str[51][51];
+int n;
+
+int check(int i, int j) {
+	int max = 1;
+	int combo = 1;
+	for (int k = i + 1; k < n; ++k) {
+		if (str[i][j] == str[k][j]) ++combo;
+		else break;
+	}
+	for (int k = i - 1; k >= 0; --k) {
+		if (str[i][j] == str[k][j]) ++combo;
+		else break;
+	}
+	if (max < combo) max = combo;
+	combo = 1;
+	for (int k = j + 1; k < n; ++k) {
+		if (str[i][j] == str[i][k]) ++combo;
+		else break;
+	}
+	for (int k = j - 1; k >= 0; --k) {
+		if (str[i][j] == str[i][k]) ++combo;
+		else break;
+	}
+	if (max < combo) max = combo;
+	return max;
+}
+void swap(char* one, char* two) {
+	char temp = *one;
+	*one = *two;
+	*two = temp;
+}
 int main() {
-	char str[51][51];
-	int n;
 	scanf_s("%d", &n);
 	for (int i = 0; i < n; ++i) {
 		getchar();
 		scanf_s("%s", &str[i][0], 51);
 	}
 	int maxCombo = 0;
+	int combo;
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j) {
-			int combo = 1;
-			bool hasMoved = false;
-			for (int k = i + 1; k < n; ++k) {
-				if (str[i][j] == str[k][j]) ++combo;
-				else if (!hasMoved) {
-					if (j + 1 < n && str[i][j] == str[k][j + 1]) ++combo;
-					else if (j && str[i][j] == str[k][j - 1]) ++combo;
-					else if (k + 1 < n && str[i][j] == str[k + 1][j]) {
-						++combo;
-						if (maxCombo < combo) maxCombo = combo;
-						combo = 1;
-					}
-					else break;
-					hasMoved = true;
-				}
-				else break;
-			}
-			if (!hasMoved) {
-				if (i && j + 1 < n && str[i][j] == str[i - 1][j + 1]) ++combo;
-				else if (i && j && str[i][j] == str[i - 1][j - 1]) ++combo;
-				else --combo;
-			}
-			if (maxCombo < combo) maxCombo = combo;
 			combo = 1;
-			hasMoved = false;
-			for (int k = j + 1; k < n; ++k) {
-				if (str[i][j] == str[i][k]) ++combo;
-				else if (!hasMoved) {
-					if (i + 1 < n && str[i][j] == str[i + 1][k]) ++combo;
-					else if (i && str[i][j] == str[i - 1][k]) ++combo;
-					else if (k + 1 < n && str[i][j] == str[i][k + 1]) {
-						++combo;
-						if (maxCombo < combo) maxCombo = combo;
-						combo = 1;
-					}
-					else break;
-					hasMoved = true;
-				}
-				else break;
+			if (i) {
+				swap(&str[i][j], &str[i - 1][j]);
+				combo = check(i, j);
+				swap(&str[i][j], &str[i - 1][j]);
+				if (combo > maxCombo) maxCombo = combo;
+				combo = 1;
 			}
-			if (!hasMoved) {
-				if (j && i + 1 < n && str[i][j] == str[i + 1][j - 1]) ++combo;
-				else if (j && i && str[i][j] == str[i - 1][j - 1]) ++combo;
-				else --combo;
+			if (i + 1 < n) {
+				swap(&str[i][j], &str[i + 1][j]);
+				combo = check(i, j);
+				swap(&str[i][j], &str[i + 1][j]);
+				if (combo > maxCombo) maxCombo = combo;
+				combo = 1;
 			}
-			if (maxCombo < combo) maxCombo = combo;
+			if (j) {
+				swap(&str[i][j], &str[i][j - 1]);
+				combo = check(i, j);
+				swap(&str[i][j], &str[i][j - 1]);
+				if (combo > maxCombo) maxCombo = combo;
+				combo = 1;
+			}
+			if (j + 1 < n) {
+				swap(&str[i][j], &str[i][j + 1]);
+				combo = check(i, j);
+				swap(&str[i][j], &str[i][j + 1]);
+				if (combo > maxCombo) maxCombo = combo;
+			}
 		}
 	}
 	printf("%d\n", maxCombo);
