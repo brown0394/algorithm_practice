f389fa257bf8e4222436cc34974632b34b7f4908
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-04-17 00:19:51 +0900

    Update adding123_4.cpp

--- adding123_4.cpp
+++ adding123_4.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
 #include <vector>
 
-struct node {
-	int possibility;
-	int twos;
-};
-
 int main() {
 	int testCases;
-	scanf_s("%d", &testCases);
-	std::vector<node> possibilities{ node{ 0, 0 }, node{1, 0}, node{ 2, 1 } };
+	scanf("%d", &testCases);
 	int in;
 	for (int i = 0; i < testCases; ++i) {
-		scanf_s("%d", &in);
-		if (in >= possibilities.size()) {
-			int j = possibilities.size();
-			possibilities.resize(in + 1);
-			for (; j <= in; ++j) {
-				possibilities[j].twos = j >> 1;
-				possibilities[j].possibility = 1 + possibilities[j].twos + (j / 3);
-				int cur = j - 3;
-				while (cur > 1) {
-					possibilities[j].possibility += possibilities[cur].twos;
-					cur -= 3;
-				}
-				if (j == 1) ++possibilities[j].possibility;
-			}
+		scanf("%d", &in);
+		int val = (in >> 1) + 1 + (in / 3);
+		int cur = in - 3;
+		while (cur > 1) {
+			val += (cur >> 1);
+			cur -= 3;
 		}
-		printf("%d\n", possibilities[in].possibility);
+        printf("%d\n", val);
 	}
 }
