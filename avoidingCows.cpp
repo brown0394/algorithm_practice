b543917f1fbde7f335d5d0a17d03efef2f06c537
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-04-29 23:17:39 +0900

    Update avoidingCows.cpp

--- avoidingCows.cpp
+++ avoidingCows.cpp
@@ -1,62 +1,65 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 #include <map>
 
 #define INVALID 2000000000
 
-bool comp(const std::pair<const int, int>* one, const std::pair<const int, int>* another) {
-	return one->second > another->second;
+struct node {
+	int to;
+	int* cost;
+	bool operator < (const node& other) {
+		return *cost > *other.cost;
+	}
+};
+
+bool operator < (const node& one, const node& another) {
+	return *one.cost > *another.cost;
 }
 
 int main() {
 	int vertex, edge;
-	scanf_s("%d %d", &vertex, &edge);
+	scanf("%d %d", &vertex, &edge);
 	std::vector<std::map<int, int>> costs(vertex + 1);
-	std::vector<std::pair<const int, int>*> heap;
+	std::priority_queue<node> heap;
+	std::vector<int> startPoint(vertex + 1, INVALID);
 	int from, to, cost;
 	for (int i = 0; i < edge; ++i) {
-		scanf_s("%d %d %d", &from, &to, &cost);
-		auto found = costs[from].find(to);
-		if (found == costs[from].end()) {
-			costs[from].insert({ to, cost });
-			costs[to].insert({ from, cost });
+		scanf("%d %d %d", &from, &to, &cost);
+		if (from == 1) {
+			if (startPoint[to] > cost) {
+				if (startPoint[to] == INVALID) heap.push({ to, &startPoint[to] });
+				startPoint[to] = cost;
+			}
 		}
-		else if (found->second > cost) {
-			found->second = cost;
-			found = costs[to].find(from);
-			found->second = cost;
+		else if (to == 1) {
+			if (startPoint[from] > cost) {
+				if (startPoint[from] == INVALID) heap.push({ from, &startPoint[from] });
+				startPoint[from] = cost;
+			}
 		}
-	}
-	int idx = 1;
-	for (auto it = costs[1].begin(); it != costs[1].end(); ++it) {
-		heap.push_back(&(*it));
-		if (it->first == idx) ++idx;
 		else {
-			for (; idx < it->first; ++idx) {
-				costs[1].insert({ idx, INVALID });
+			auto found = costs[from].find(to);
+			if (found == costs[from].end()) {
+				costs[from].insert({ to, cost });
+				costs[to].insert({ from, cost });
+			}
+			else if (found->second > cost) {
+				found->second = cost;
+				found = costs[to].find(from);
+				found->second = cost;
 			}
 		}
 	}
-	for (; idx <= vertex; ++idx) {
-		costs[1].insert({ idx, INVALID });
-	}
-	std::make_heap(heap.begin(), heap.end(), comp);
 	while (!heap.empty()) {
-		std::pop_heap(heap.begin(), heap.end(), comp);
-		std::pair<const int, int>* cur = heap.back();
-		heap.pop_back();
-		bool change = false;
-		for (auto it = costs[cur->first].begin(); it != costs[cur->first].end(); ++it) {
-			if (it->first == 1) continue;
-			auto found = costs[1].find(it->first);
-			if (found->second > cur->second + it->second) {
-				if (found->second == INVALID) heap.push_back(&(*found));
-				found->second = cur->second + it->second;
-				change = true;
+		node cur = heap.top();
+		heap.pop();
+		for (auto it = costs[cur.to].begin(); it != costs[cur.to].end(); ++it) {
+			if (startPoint[it->first] > *cur.cost + it->second) {
+				heap.push({ it->first, &startPoint[it->first] });
+				startPoint[it->first] = *cur.cost + it->second;
 			}
 		}
-		if (change) std::make_heap(heap.begin(), heap.end(), comp);
 	}
-	printf("%d\n", costs[1][vertex]);
+	printf("%d\n", startPoint[vertex]);
 }
