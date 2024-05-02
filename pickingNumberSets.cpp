8d67e11e10605b749f6142ab8afc8b11908f7e79
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-05-02 23:20:42 +0900

    Update pickingNumberSets.cpp

--- pickingNumberSets.cpp
+++ pickingNumberSets.cpp
@@ -7,19 +7,14 @@ int main() {
 	scanf_s("%d", &n);
 	std::vector<int> upper(n + 1);
 	std::bitset<101> visited;
-	std::vector<int> baseArr;
-	baseArr.reserve(50);
+	std::bitset<101> added;
 	for (int i = 1; i <= n; ++i) {
 		scanf_s("%d", &upper[i]);
-		if (upper[i] == i) {
-			baseArr.push_back(i);
-			upper[i] = 0;
-		}
 	}
 	int maxLen = 0;
-	int maxStart = 0;
 	int start;
 	for (int i = 1; i <= n; ++i) {
+		if (visited[i]) continue;
 		start = i;
 		int cur = upper[i];
 		int len = 0;
@@ -27,31 +22,19 @@ int main() {
 		while (cur) {
 			++len;
 			if (cur == start) {
-				if (maxLen < len) {
-					maxLen = len;
-					maxStart = start;
-				}
+				maxLen += len;
+				added = visited;
 				break;
 			}
 			else if (visited[cur]) break;
 			visited[cur] = true;
 			cur = upper[cur];
 		}
-		visited = 0;
+		visited = added;
 	}
-	int baseIdx = 0;
-	int size = baseArr.size();
-	int cur = maxStart;
-	printf("%d\n", maxLen + size);
-	while (cur) {
-		if (baseIdx < size && baseArr[baseIdx] < cur) {
-			printf("%d\n", baseArr[baseIdx++]);
-		}
-		else {
-			printf("%d\n", cur);
-			if (upper[cur] == maxStart) break;
-			cur = upper[cur];
-		}
+	printf("%d\n", maxLen);
+	for (int i = 1; i < 101; ++i) {
+		if (visited[i]) printf("%d\n", i);
 	}
-	for (; baseIdx < size; ++baseIdx) printf("%d\n", baseArr[baseIdx]);
+	
 }
