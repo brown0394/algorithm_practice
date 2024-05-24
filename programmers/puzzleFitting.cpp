3cd869af70312c370501feec9ce347dc0dfccd3b
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-05-24 23:07:20 +0900

    Update puzzleFitting.cpp

--- programmers/puzzleFitting.cpp
+++ programmers/puzzleFitting.cpp
@@ -1,199 +1,178 @@
-#include <queue>
+#include <string>
 #include <vector>
+#include <queue>
+#include <list>
+#include <algorithm>
+#define BLOCK 5
 
-#define BLOCK 20
 using namespace std;
 
-struct node {
-    int blocks;
-    vector<vector<int>> shape;
+struct blockInfo {
+    unsigned char shape[6];
+    int blocks, row, col;
 };
 
-void bfs(vector<vector<int>>& table, node& info, queue<pair<int, int>>& q, const int block) {
-    int i, j, row = table.size(), col = table[0].size();
-    int sRow = q.front().first, sCol = q.front().second;
-    int sRowE = sRow + 1, sColE = sCol + 1;
+blockInfo bfs(int i, int j, vector<vector<int>>& table, const int block) {
+    queue<pair<int, int>> q;
+    blockInfo info{ {0}, 0, 0, 0 };
+    q.push({ i, j });
+    int row = table.size();
+    int col = table[0].size();
+    int srow = i, scol = j;
+    table[i][j] = BLOCK;
     while (!q.empty()) {
         i = q.front().first;
         j = q.front().second;
         q.pop();
+        ++info.blocks;
+        info.shape[i - srow] |= 1 << (7 - (j-scol));
+        if (i + 1 < row && table[i + 1][j] == block) {
+            if (i - srow == info.row) ++info.row;
+            table[i + 1][j] = BLOCK;
+            q.push({ i + 1, j });
+        }
         if (i && table[i - 1][j] == block) {
             table[i - 1][j] = BLOCK;
+            if (i == srow) {
+                --srow;
+                ++info.row;
+                for (int k = 5; k >= 1; --k) info.shape[k] = info.shape[k - 1];
+                info.shape[0] = 0;
+            }
             q.push({ i - 1, j });
-            ++info.blocks;
-            info.shape[i - 1 - sRow][j - sCol] = 1;
+        }
+        if (j + 1 < col && table[i][j + 1] == block) {
+            if (j - scol == info.col) ++info.col;
+            table[i][j + 1] = BLOCK;
+            q.push({ i, j + 1 });
         }
         if (j && table[i][j - 1] == block) {
             table[i][j - 1] = BLOCK;
+            if (j == scol) {
+                --scol;
+                ++info.col;
+                for (int k = 0; k < 6; ++k) info.shape[k] >>= 1;
+            }
             q.push({ i, j - 1 });
-            ++info.blocks;
-            if (j - 1 < sCol) {
-                for (auto it = info.shape.begin(); it != info.shape.end(); ++it) {
-                    it->insert(it->begin(), 0);
-                }
-                --sCol;
-            }
-            info.shape[i - sRow][j - sCol - 1] = 1;
         }
-        if (i + 1 < row && table[i + 1][j] == block) {
-            table[i + 1][j] = BLOCK;
-            q.push({ i + 1, j });
-            ++info.blocks;
-            if (i + 1 == sRowE) {
-                info.shape.push_back(vector<int>(sColE - sCol, 0));
-                ++sRowE;
+    }
+    return info;
+}
+
+bool compareBlock(blockInfo& one, blockInfo& two) {
+    for (int i = 0; i <= one.row; ++i) {
+        if (one.shape[i] != two.shape[i]) return false;
+    }
+    return true;
+}
+
+void rotate90(blockInfo& block) {
+    unsigned char temp[6]{ 0 };
+    int tempCur = 1 << 7;
+    for (int i = block.row; i >= 0; --i) {
+        int cur = 1 << 7;
+        for (int j = 0; j <= block.col; ++j) {
+            if (cur & block.shape[i]) {
+                temp[j] |= tempCur;
             }
-            info.shape[i - sRow + 1][j - sCol] = 1;
+            cur >>= 1;
         }
-        if (j + 1 < col && table[i][j + 1] == block) {
-            table[i][j + 1] = BLOCK;
-            q.push({ i, j + 1 });
-            ++info.blocks;
-            if (j + 1 == sColE) {
-                for (auto it = info.shape.begin(); it != info.shape.end(); ++it) {
-                    it->push_back(0);
-                }
-                ++sColE;
-            }
-            info.shape[i - sRow][j - sCol + 1] = 1;
-        }
+        tempCur >>= 1;
+    }
+    int rc = block.row;
+    block.row = block.col;
+    block.col = rc;
+    for (int i = 0; i <= block.row; ++i) {
+        block.shape[i] = temp[i];
     }
 }
 
-void getBlockInfo(vector<vector<int>>& table, vector<node>& info, const int block) {
-    for (int i = 0, len = table.size(); i < len; ++i) {
-        for (int j = 0, col = table[i].size(); j < col; ++j) {
-            if (table[i][j] == block) {
-                table[i][j] = BLOCK;
-                queue<pair<int, int>> q;
-                q.push({ i, j });
-                info.push_back(node{ 1, vector<vector<int>>(1, vector<int>(1, 1))});
-                bfs(table, info.back(), q, block);
+void rotate180(blockInfo& block) {
+    unsigned char temp[6]{ 0 };
+    int idx = 0;
+    for (int i = block.row; i >= 0; --i) {
+        int cur = 1 << 7 - block.col;
+        int tempCur = 1 << 7;
+        for (int j = 0; j <= block.col; ++j) {
+            if (cur & block.shape[i]) {
+                temp[idx] |= tempCur;
             }
+            cur <<= 1;
+            tempCur >>= 1;
         }
+        ++idx;
+    }
+    for (int i = 0; i <= block.row; ++i) {
+        block.shape[i] = temp[i];
     }
 }
 
-void rotate90Right(node& info) {
-    int len = info.shape.size()-1;
-    vector<vector<int>> origin(info.shape);
-    for (int i = 0; i <= len; ++i) {
-        for (int j = 0; j <= len; ++j) {
-            info.shape[i][j] = origin[len - j][i];
+bool compareBlocks(blockInfo& one, blockInfo& two) {
+    bool result = false;
+    if (one.row == two.row && one.col == two.col) {
+        result = compareBlock(one, two);
+        if (result) return true;
+        if (one.row == one.col) {
+            for (int i = 0; i < 3; ++i) {
+                rotate90(two);
+                result = compareBlock(one, two);
+                if (result) return true;
+            }
+        }
+        else {
+            rotate180(two);
+            result = compareBlock(one, two);
         }
     }
+    else if (one.row == two.col && one.col == two.row) {
+        rotate90(two);
+        result = compareBlock(one, two);
+        if (result) return true;
+        rotate180(two);
+        result = compareBlock(one, two);
+    }
+    return result;
 }
 
-void rotate90Rect(node& info) {
-    int row = info.shape.size() - 1;
-    int col = info.shape[0].size() - 1;
-    vector<vector<int>> origin(info.shape);
-    if (row > col) {
-        for (int i = 0; i <= col; ++i) {
-            info.shape[i].resize(row + 1);
+void printBlock(blockInfo& block) {
+    for (int k = 0; k <= block.row; ++k) {
+        int check = 1 << 7;
+        for (int l = 0; l <= block.col; ++l) {
+            if (block.shape[k] & check) printf("1");
+            else printf("0");
+            check >>= 1;
         }
-        for (int i = 0; i <= col; ++i) {
-            for (int j = 0; j <= row; ++j) {
-                info.shape[i][j] = origin[row - j][i];
-            }
-        }
-        info.shape.resize(col + 1);
+        printf("\n");
     }
-    else {
-        info.shape.resize(col + 1);
-        for (int i = row + 1; i <= col; ++i) {
-            info.shape[i].resize(row + 1);
-        }
-        for (int i = 0; i <= row; ++i) {
-            for (int j = 0; j <= col; ++j) {
-                info.shape[j][i] = origin[row - i][j];
-            }
-        }
-        for (int i = 0; i <= row; ++i) {
-            info.shape[i].resize(row + 1);
-        }
-    }
-}
-
-void rotate180(node& info) {
-    int row = info.shape.size() - 1;
-    int col = info.shape[0].size() - 1;
-    vector<vector<int>> origin(info.shape);
-    for (int i = 0; i <= row; ++i) {
-        for (int j = 0; j <= col; ++j) {
-            info.shape[i][j] = origin[row - i][col - j];
-        }
-    }
-}
-
-bool fitting(vector<node>& gbInfos, vector<node>& tbInfos, int gidx, int tidx) {
-    int gRow = gbInfos[gidx].shape.size()-1;
-    int gCol = gbInfos[gidx].shape[0].size()-1;
-    int tRow = tbInfos[tidx].shape.size()-1;
-    int tCol = tbInfos[tidx].shape[0].size()-1;
-    bool found;
-    if (gRow == tRow && gCol == tCol) {
-        for (int k = 0; k < 4; ++k) {
-            found = true;
-            for (int i = 0; i <= gRow; ++i) {
-                for (int j = 0; j <= gCol; ++j) {
-                    if (gbInfos[gidx].shape[i][j] != tbInfos[tidx].shape[i][j]) {
-                        found = false;
-                        break;
-                    }
-                }
-                if (!found) break;
-            }
-            if (found) return true;
-            rotate90Right(tbInfos[tidx]);
-        }
-
-    }
-    else if (gCol == tRow && gRow == tCol) {
-        rotate90Rect(tbInfos[tidx]);
-        for (int k = 0; k < 2; ++k) {
-            found = true;
-            for (int i = 0; i <= gRow; ++i) {
-                for (int j = 0; j <= gCol; ++j) {
-                    if (gbInfos[gidx].shape[i][j] != tbInfos[tidx].shape[i][j]) {
-                        found = false;
-                        break;
-                    }
-                }
-                if (!found) break;
-            }
-            if (found) return true;
-            rotate180(tbInfos[tidx]);
-        }
-    }
-    return false;
-}
-
-int blocksFit(vector<node>& gbInfos, vector<node>& tbInfos) {
-    int sum = 0;
-    int tlen = tbInfos.size(), glen = gbInfos.size();
-    for (int i = 0; i < glen; ++i) {
-        for (int j = 0; j < tlen; ++j) {
-            if (gbInfos[i].blocks == tbInfos[j].blocks && fitting(gbInfos, tbInfos, i, j)) {
-                sum += gbInfos[i].blocks;
-                tbInfos[j].blocks = 0;
-                break;
-            }
-        }
-    }
-    return sum;
+    printf("\n");
 }
 
 int solution(vector<vector<int>> game_board, vector<vector<int>> table) {
-    vector<node> gbInfos;
-    getBlockInfo(game_board, gbInfos, 0);
-    vector<node> tbInfos;
-    getBlockInfo(table, tbInfos, 1);
-    return blocksFit(gbInfos, tbInfos);
-}
-
-int main() {
-    vector<vector<int>> game_board{ {1, 0, 1}, {0,0,0}, {0, 1, 1} };
-    vector<vector<int>> table{ {1, 1, 0}, {1, 1, 0}, {0, 1, 0} };
-    solution(game_board, table);
+    int answer = 0;
+    vector<list<blockInfo>> blocks(7);
+    blockInfo block;
+    for (int i = 0, row = table.size(); i < row; ++i) {
+        for (int j = 0, col = table[i].size(); j < col; ++j) {
+            if (table[i][j] == 1) {
+                block = bfs(i, j, table, 1);
+                blocks[block.blocks].emplace_back(move(block));
+            }
+        }
+    }
+    
+    for (int i = 0, row = game_board.size(); i < row; ++i) {
+        for (int j = 0, col = game_board[i].size(); j < col; ++j) {
+            if (!game_board[i][j]) {
+                block = bfs(i, j, game_board, 0);
+                for (auto it = blocks[block.blocks].begin(); it != blocks[block.blocks].end(); ++it) {
+                    if (compareBlocks(block, *it)) {
+                        answer += block.blocks;
+                        blocks[block.blocks].erase(it);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return answer;
 }
