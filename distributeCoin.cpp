2bd026c4e1b55ad537a165c5ffff22370f7b54c8
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-05-29 00:10:52 +0900

    Update distributeCoin.cpp

--- distributeCoin.cpp
+++ distributeCoin.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <stack>
 
 struct node {
     int num;
@@ -11,21 +9,34 @@ struct node {
     }
 };
 
+bool findCombination(int target, int sum, int i, std::vector<node>& coins) {
+    if (i == coins.size()) return false;
+    for (int j = 0; j < coins[i].count; ++j) {
+        sum += coins[i].num;
+        if (sum == target) return true;
+        if (sum < target && findCombination(target, sum, i + 1, coins)) return true;
+    }
+    return false;
+}
+
 int main() {
     std::vector<node> coins;
     for (int i = 0; i < 3; ++i) {
         int n;
         scanf_s("%d", &n);
         coins.resize(n);
+        int sum = 0;
         for (int j = 0; j < n; ++j) {
             scanf_s("%d %d", &coins[j].num, &coins[j].count);
+            sum += (coins[j].num * coins[j].count);
         }
-        std::sort(coins.begin(), coins.end());
-        for (int j = 0; j < n; ++j) {
-            std::stack<int> stk;
-            if (coins[j].count % 2) {
-
-            }
+        if (sum % 2) {
+            printf("0\n");
+            coins.clear();
+            continue;
         }
+        sum >>= 1;
+        if (findCombination(sum, 0, 0, coins)) printf("1\n");
+        else printf("0\n");
     }
 }
