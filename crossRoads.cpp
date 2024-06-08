e968a4ece6b4b689236a1adca11a8c0c984540fe
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-06-08 22:47:23 +0900

    Update crossRoads.cpp

--- crossRoads.cpp
+++ crossRoads.cpp
@@ -1,52 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <queue>
 
+const long long inf = std::numeric_limits<long long>::max();
 struct node {
-	int to, idx;
+	node(long long p_cost = inf, int p_idx = -1, int p_to = -1) :
+		cost(p_cost), idx(p_idx), to(p_to) {}
+	long long cost;
+	int idx;
+	int to;
 };
 std::vector<std::vector<node>> infos;
+std::vector<node> startP;
 int regions, period;
-const long long inf = std::numeric_limits<long long>::max();
-long long getMinTime(int from, int idx, long long sum) {
-	if (from == 1) return sum + idx;
-	long long min = inf;
-	long long cur = inf;
-	for (auto it = infos[from].begin(), end = infos[from].end(); it != end; ++it) {
-		long long next;
-		if (it->idx < idx) {
-			next = idx - it->idx;
-		}
-		else {
-			next = (idx + period) - it->idx;
-		}
-		next += sum;
-		cur = getMinTime(it->to, it->idx, next);
-		if (cur < min) {
-			printf("%d %d\n", it->idx, idx);
-			min = cur;
-		}
-	}
-	return min;
-}
-int main() {
-	
-	scanf_s("%d %d", &regions, &period);
 
-	infos.resize(regions + 1, std::vector<node>((period / regions) + 1));
+int main() {
+	scanf_s("%d %d", &regions, &period);
+	infos.resize(regions + 1);
+	startP.resize(regions + 1);
 	int from, to;
+	std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
 	for (int i = 1; i <= period; ++i) {
 		scanf_s("%d %d", &from, &to);
-		if (from > to) infos[from].emplace_back(node{ to, i });
-		else infos[to].emplace_back(node{ from, i });
+		if (from == 1) {
+			if (startP[to].cost < inf) continue;
+			startP[to].cost = startP[to].idx = i;
+			pq.push(to);
+		}
+		else if (to == 1) {
+			if (startP[from].cost < inf) continue;
+			startP[from].cost = startP[from].idx = i;
+			pq.push(from);
+		}
+		else {
+			infos[from].emplace_back(i, i, to);
+			infos[to].emplace_back(i, i, from);
+		}
 	}
-	long long min = inf;
-	long long cur = inf;
-	for (auto it = infos[regions].begin(), end = infos[regions].end(); it != end; ++it) {
-		printf("\n");
-		cur = getMinTime(it->to, it->idx, 0);
-		if (cur < min) min = cur;
-		printf("%d\n", min);
+	startP[1].cost = 0;
+	while (!pq.empty()) {
+		int idx = pq.top();
+		pq.pop();
+		for (auto it = infos[idx].begin(), end = infos[idx].end(); it != end; ++it) {
+			long long toCost = 0;
+			if (it->idx > startP[idx].idx) {
+				toCost = (it->idx - startP[idx].idx);
+			}
+			else {
+				toCost = ((it->idx + period) - startP[idx].idx);
+			}
+			toCost += startP[idx].cost;
+			if (toCost < startP[it->to].cost) {
+				startP[it->to].cost = toCost;
+				startP[it->to].idx = it->idx;
+				pq.push(it->to);
+			}
+		}
 	}
-	printf("%lld\n", min);
+
+	printf("%lld\n", startP[regions].cost);
 }
