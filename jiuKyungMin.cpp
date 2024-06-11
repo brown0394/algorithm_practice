8ffd1d9ee89646ca7aa25d9e5b6f024dbf834f00
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-06-11 22:50:21 +0900

    Update jiuKyungMin.cpp

--- jiuKyungMin.cpp
+++ jiuKyungMin.cpp
@@ -1,99 +1,107 @@
 #include <iostream>
 #include <vector>
 
-struct status {
-	int idx, wins;
-};
-
-const int players = 3;
-const char jiu = 2;
-const char kyung = 0;
-const char min = 1;
-const int jidx = 0, win = 2;
+const int win = 2;
+const char jiu = 1;
+const char kyung = 2;
+const char min = 4;
 int nItems, winN, allkind;
-status curStatus[3] { 0 };
+int jwins = 0, kwins = 0, mwins = 0, kidx = 0, midx = 0;
 std::vector<std::vector<int>> wdl;
-std::vector<std::vector<int>> itemUsed;
-bool canWin(int used, int winner, int challenger) {
+std::vector<std::vector<int>> kymin;
+bool canWin(char nowPlaying, int used) {
+	if (midx == 20 || kidx == 20) return false;
 	if (!used) printf("\n-----------\n");
-	printf("kw %d, mw %d, jw %d\n", curStatus[kyung].wins, curStatus[min].wins, curStatus[jiu].wins);
+	printf("jw %d, kw %d, mw %d\n", jwins, kwins, mwins);
+	char next = kyung;
+	int cur = 1;
+	int* curIdx = &midx;
 	bool result = false;
-	if (winner < jiu && challenger < jiu) {
-		printf("p1 = %d, %d | p2 = %d, %d | r %d\n", winner, itemUsed[winner][curStatus[winner].idx], challenger,
-			itemUsed[challenger][curStatus[challenger].idx], wdl[itemUsed[winner][curStatus[winner].idx]][itemUsed[challenger][curStatus[challenger].idx]]);
-		if (wdl[itemUsed[winner][curStatus[winner].idx]][itemUsed[challenger][curStatus[challenger].idx]] == win) {
-			if (curStatus[winner].wins + 1 == winN) return false;
-			++curStatus[winner].idx;
-			++curStatus[challenger].idx;
-			++curStatus[winner].wins;
-			result = canWin(used, winner, jiu);
-			--curStatus[winner].wins;
+	if (nowPlaying == 3) {
+		next = min;
+		cur = 0;
+		curIdx = &kidx;
+	}
+	else if (nowPlaying == 6){
+		int* wins;
+		printf("k %d, m %d result : %d\n\n", kymin[0][kidx], kymin[1][midx], wdl[kymin[0][kidx]][kymin[1][midx]]);
+		if (wdl[kymin[0][kidx]][kymin[1][midx]] == win) {
+			next = kyung;
+			wins = &kwins;
 		}
 		else {
-			if (curStatus[challenger].wins + 1 == winN) return false;
-			++curStatus[winner].idx;
-			++curStatus[challenger].idx;
-			++curStatus[challenger].wins;
-			result = canWin(used, challenger, jiu);
-			--curStatus[challenger].wins;
+			next = min;
+			wins = &mwins;
 		}
-		--curStatus[winner].idx;
-		--curStatus[challenger].idx;
+		if (*wins + 1 == winN) return false;
+		++kidx;
+		++midx;
+		++(*wins);
+		result = canWin(next | jiu, used);
+		--(*wins);
+		--kidx;
+		--midx;
+		printf("ret\n");
 		return result;
 	}
-	bool jiuFirst = winner == jiu;
+	int curItem = 1;
 	for (int i = 1; i <= nItems; ++i) {
-		curStatus[jiu].idx = i;
-		printf("p1 = %d, %d | p2 = %d, %d | r %d\n", winner, itemUsed[winner][curStatus[winner].idx], challenger,
-			itemUsed[challenger][curStatus[challenger].idx], wdl[itemUsed[winner][curStatus[winner].idx]][itemUsed[challenger][curStatus[challenger].idx]]);
-		if (wdl[itemUsed[winner][curStatus[winner].idx]][itemUsed[challenger][curStatus[challenger].idx]] == win) {
-			if (curStatus[winner].wins + 1 == winN) {
-				if (jiuFirst && (used | (1 << (i-1))) == allkind) return true;
+		if (!(used & curItem)) {
+			char op;
+			if (cur) op = 'm';
+			else op = 'k';
+			printf("j %d, %c %d result: %d\n\n", i, op, kymin[cur][*curIdx], wdl[i][kymin[cur][*curIdx]]);
+			if (wdl[i][kymin[cur][*curIdx]] == win) {
+				if (jwins + 1 == winN) {
+					return true;
+				}
+				++jwins;
+				++(*curIdx);
+				result = canWin(next | jiu, used | curItem);
+				--jwins;
+				--(*curIdx);
+				printf("ret\n");
 			}
 			else {
-				++curStatus[winner].wins;
-				++curStatus[winner].idx;
-				result = canWin(used | (1 << (i - 1)), winner, 3 - winner - challenger);
-				--curStatus[winner].wins;
-				--curStatus[winner].idx;
+				int* wins;
+				int winner;
+				int* idx;
+				if (cur) {
+					winner = min;
+					idx = &midx;
+					wins = &mwins;
+				}
+				else {
+					winner = kyung;
+					idx = &kidx;
+					wins = &kwins;
+				}
+				if (*wins + 1 < winN) {
+					++(*wins);
+					++(*idx);
+					result = canWin(next | winner, used | curItem);
+					--(*wins);
+					--(*idx);
+					printf("ret\n");
+				}
 			}
+			if (result) return true;
 		}
-		else {
-			if (curStatus[challenger].wins + 1 == winN) {
-				if (!jiuFirst && (used | (1 << (i - 1))) == allkind) return true;
-			}
-			else {
-				++curStatus[challenger].wins;
-				++curStatus[challenger].idx;
-				result = canWin(used | (1 << (i - 1)), challenger, 3 - winner - challenger);
-				--curStatus[challenger].wins;
-				--curStatus[challenger].idx;
-			}
-		}
-		if (result) break;
+		curItem <<= 1;
 	}
 	return result;
 }
 
 int main() {
 	scanf_s("%d %d", &nItems, &winN);
-	int curItem = 1;
-	for (int i = 0; i < nItems; ++i) {
-		allkind |= curItem;
-		curItem <<= 1;
-	}
 	wdl.resize(nItems + 1, std::vector<int>(nItems + 1));
 	for (int i = 1; i <= nItems; ++i) {
 		for (int j = 1; j <= nItems; ++j) scanf_s("%d", &wdl[i][j]);
 	}
-	itemUsed.resize(2, std::vector<int>(20));
+	kymin.resize(2, std::vector<int>(20));
 	for (int i = 0; i < 2; ++i) {
-		for (int j = 0; j < 20; ++j) scanf_s("%d", &itemUsed[i][j]);
+		for (int j = 0; j < 20; ++j) scanf_s("%d", &kymin[i][j]);
 	}
-	itemUsed.push_back(std::vector<int>(nItems+1));
-	for (int i = 0; i <= nItems; ++i) {
-		itemUsed[jiu][i] = i;
-	}
-	if (canWin(0, 2, 0)) printf("1\n");
+	if (canWin(jiu | kyung, 0)) printf("1\n");
 	else printf("0\n");
 }
