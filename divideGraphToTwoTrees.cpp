746064fc8c665a3e8c590a3ed9d8586cd1395c2e
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-06-30 23:12:09 +0900

    Update divideGraphToTwoTrees.cpp

--- divideGraphToTwoTrees.cpp
+++ divideGraphToTwoTrees.cpp
@@ -1,125 +1,88 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <queue>
-#include <list>
-#include <deque>
 
-struct node {
-	int root;
-	std::list<int> children;
-	std::list<int> edges;
-};
-
-void adoptTree(std::vector<node>& nodeInfos, int newRoot, int oldRoot, int newEdgeN) {
-	while (!nodeInfos[oldRoot].children.empty()) {
-		nodeInfos[nodeInfos[oldRoot].children.back()].root = newRoot;
-		nodeInfos[newRoot].children.push_back(nodeInfos[oldRoot].children.back());
-		nodeInfos[oldRoot].children.pop_back();
-	}
-	nodeInfos[newRoot].edges.splice(nodeInfos[newRoot].edges.end(), nodeInfos[oldRoot].edges);
-	nodeInfos[newRoot].edges.push_back(newEdgeN);
-}
-
-void print(std::list<int>& v) {
+void print(std::vector<int>& v) {
 	printf("\n");
 	if (v.empty()) return;
-	v.sort();
+	std::sort(v.begin(), v.end());
 	auto it = v.begin();
 	printf("%d", *it++);
 	for (auto end = v.end(); it != end; ++it) printf(" %d", *it);
+}
+std::vector<bool> visited;
+struct node {
+	int to, edgeN;
+};
+
+void bfs(std::vector<int>& nodes, std::vector<int>& edges, std::vector<std::vector<node>>& connections, int nodeN) {
+	nodes.push_back(nodeN);
+	std::queue<int> q;
+	q.push(nodeN);
+	while (!q.empty()) {
+		nodeN = q.front();
+		q.pop();
+		for (auto elems : connections[nodeN]) {
+			if (!visited[elems.to]) {
+				visited[elems.to] = true;
+				q.push(elems.to);
+				nodes.push_back(elems.to);
+				edges.push_back(elems.edgeN);
+			}
+		}
+	}
 }
 
 int main() {
 	int nodes, edges;
 	scanf_s("%d %d", &nodes, &edges);
-	std::vector<node> nodeInfos(nodes + 1, { 0 });
-	std::vector<int> visitCount(nodes + 1);
-	std::vector<std::pair<int, int>> edgeArr(edges + 1);
-	int trees = 0;
-	int nodesTaken = 0;
+	std::vector<std::vector<node>> connections(nodes + 1);
+	visited.resize(nodes + 1, false);
+	std::vector<int> nodes1;
+	std::vector<int> edges1;
+	std::vector<int> nodes2;
+	std::vector<int> edges2;
 	int from, to;
 	for (int i = 1; i <= edges; ++i) {
 		scanf_s("%d %d", &from, &to);
-		edgeArr[i].first = from;
-		edgeArr[i].second = to;
-		if (nodeInfos[from].root && nodeInfos[to].root) {
-			int root1 = nodeInfos[from].root, root2 = nodeInfos[to].root;
-			if (root1 == root2) continue;
-			if (nodeInfos[root1].children.size() >= nodeInfos[root2].children.size()) {
-				adoptTree(nodeInfos, nodeInfos[from].root, nodeInfos[to].root, i);
-			}
-			else {
-				adoptTree(nodeInfos, nodeInfos[to].root, nodeInfos[from].root, i);
-			}
-			--trees;
-			continue;
-		}
-		else if (!nodeInfos[from].root && nodeInfos[to].root) {
-			std::swap(from, to);
-		}
-		else if (!nodeInfos[from].root && !nodeInfos[to].root) {
-			if (from > to) {
-				std::swap(from, to);
-			}
-			nodeInfos[from].root = from;
-			nodeInfos[from].children.push_back(from);
-			if (!visitCount[to] && !visitCount[from]) ++trees;
-		}
-
-		nodeInfos[to].root = nodeInfos[from].root;
-		nodeInfos[nodeInfos[from].root].children.push_back(to);
-		nodeInfos[nodeInfos[from].root].edges.push_back(i);
-		if (!visitCount[to]++) ++nodesTaken;
-		if (!visitCount[from]++) ++nodesTaken;
-
+		connections[from].push_back({ to, i });
+		connections[to].push_back({ from, i });
+		
 	}
-	trees += (nodes - nodesTaken);
-	if (trees > 2) {
+	if (nodes < 3) {
 		printf("-1\n");
 		return 0;
-	}
-	else if (trees == 1) {
-		for (auto it = nodeInfos[nodeInfos[1].root].edges.begin(),
-			end = nodeInfos[nodeInfos[1].root].edges.end(); it != end; ++it) {
-			int leaf = 0;
-			if (visitCount[edgeArr[*it].first] == 1 && nodeInfos[edgeArr[*it].first].children.empty()) {
-				leaf = edgeArr[*it].first;
-			}
-			else if (visitCount[edgeArr[*it].second] == 1 && nodeInfos[edgeArr[*it].second].children.empty()) {
-				leaf = edgeArr[*it].second;
-			}
-			if (leaf) {
-				int p = nodeInfos[leaf].root;
-				nodeInfos[leaf].root = leaf;
-				nodeInfos[leaf].children.push_back(leaf);
-				nodeInfos[p].children.remove(leaf);
-				nodeInfos[p].edges.erase(it);
-				break;
-			}
-		}
 	}
 	int one = 0, two = 0;
 	for (int i = 1; i <= nodes; ++i) {
-		if (!nodeInfos[i].root) {
-			nodeInfos[i].root = i;
-			nodeInfos[i].children.push_back(i);
-		}
-		if (nodeInfos[i].root == i) {
-			if (!one) one = i;
+		if (!visited[i]) {
+			visited[i] = true;
+			if (nodes1.empty()) {
+				bfs(nodes1, edges1, connections, i);
+			}
+			else if (nodes2.empty()) {
+				bfs(nodes2, edges2, connections, i);
+			}
 			else {
-				two = i;
-				break;
+				printf("-1\n");
+				return 0;
 			}
 		}
 	}
-	if (nodeInfos[one].children.size() == nodeInfos[two].children.size() || !two) {
+	if (nodes2.empty()) {
+		nodes2.push_back(nodes1.back());
+		nodes1.pop_back();
+		edges1.pop_back();
+	}
+	else if (nodes1.size() == nodes2.size()) {
 		printf("-1\n");
 		return 0;
 	}
-	printf("%d %d", nodeInfos[one].children.size(), nodeInfos[two].children.size());
-	print(nodeInfos[one].children);
-	print(nodeInfos[one].edges);
-	print(nodeInfos[two].children);
-	print(nodeInfos[two].edges);
-	putchar('\n');
+	printf("%d %d", nodes1.size(), nodes2.size());
+	print(nodes1);
+	print(edges1);
+	print(nodes2);
+	print(edges2);
+	printf("\n");
 }
