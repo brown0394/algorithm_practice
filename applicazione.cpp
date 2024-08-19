14e1e5ca33691a64f375b4d8d5c226ad894a7aa6
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-08-19 22:48:17 +0900

    Update applicazione.cpp

--- applicazione.cpp
+++ applicazione.cpp
@@ -2,37 +2,31 @@
 #include <vector>
 #include <algorithm>
 
-struct node {
-	int mem, cost;
-};
-
 int main() {
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);	
 	int runningApps, memNeeded;
-	const int MAX = 1000000000;
 	std::cin >> runningApps >> memNeeded;
-	std::vector<node> appInfos(runningApps + 1);
-	for (int i = 1; i <= runningApps; ++i) std::cin >> appInfos[i].mem;
-	for (int i = 1; i <= runningApps; ++i) std::cin >> appInfos[i].cost;
-	std::vector<std::vector<int>> knapsackDP(memNeeded + 1, std::vector<int>(runningApps + 1, MAX));
-	for (int mem = 1; mem <= memNeeded; ++mem) {
+	std::vector<int> memInfo(runningApps + 1);
+	std::vector<int> costInfo(runningApps + 1);
+	for (int i = 1; i <= runningApps; ++i) std::cin >> memInfo[i];
+	int maxCost = 0;
+	for (int i = 1; i <= runningApps; ++i) {
+		std::cin >> costInfo[i];
+		maxCost += costInfo[i];
+	}
+	
+	std::vector<std::vector<int>> knapsackDP(maxCost + 1, std::vector<int>(runningApps + 1, 0));
+	for (int cost = 0; cost <= maxCost; ++cost) {
 		for (int i = 1; i <= runningApps; ++i) {
-			knapsackDP[mem][i] = knapsackDP[mem][i - 1];
-			if (mem >= appInfos[i].mem) {
-				if (knapsackDP[mem - appInfos[i].mem][i - 1] == MAX) {
-					if (appInfos[i].cost < knapsackDP[mem][i]) {
-						knapsackDP[mem][i] = appInfos[i].cost;
-					}
+			knapsackDP[cost][i] = knapsackDP[cost][i - 1];
+			if (costInfo[i] <= cost && knapsackDP[cost][i] < knapsackDP[cost - costInfo[i]][i - 1] + memInfo[i]) {
+				knapsackDP[cost][i] = knapsackDP[cost - costInfo[i]][i - 1] + memInfo[i];
+				if (knapsackDP[cost][i] >= memNeeded) {
+					std::cout << cost << '\n';
+					return 0;
 				}
-				else {
-					if (knapsackDP[mem][i] > knapsackDP[mem - appInfos[i].mem][i - 1] + appInfos[i].cost) {
-						knapsackDP[mem][i] = knapsackDP[mem - appInfos[i].mem][i - 1] + appInfos[i].cost;
-					}
-				}
-
 			}
 		}
 	}
-	std::cout << knapsackDP[memNeeded][runningApps] << '\n';
 }
