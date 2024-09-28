44f1db37350d34b812c93a8fc068b59e5de6d2c3
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-09-28 12:41:39 +0900

    Update isPalindrome.cpp

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
 #include <vector>
 
-enum EPalin{
-	needCheck = 0,
-	palind,
-	nonPalind
-};
-
 int main() {
 	std::ios::sync_with_stdio(0);
 	std::cin.tie(0);
 	int len;
 	std::cin >> len;
-	std::vector<int> arr(len + 1);
-	std::vector<std::vector<int>> isPalindrome(len + 1, std::vector<int>(len + 1, EPalin::needCheck));
-	for (int i = 1; i <= len; ++i) {
-		std::cin >> arr[i];
-		isPalindrome[i][i] = EPalin::palind;
+	len = (len << 1) + 1;
+	std::vector<int> manacherArr(len);
+	manacherArr[0] = -1;
+	for (int i = 1; i < len; i += 2) {
+		std::cin >> manacherArr[i];
+		manacherArr[i + 1] = -1;
 	}
-	int queries;
-	std::cin >> queries;
-	for (int i = 0; i < queries; ++i) {
-		int from, to;
-		std::cin >> from >> to;
-		int palindrome = 1;
-		int l = from, r = to;
-		while (true) {
-			if (isPalindrome[l][r] == EPalin::needCheck) {
-				if (r - l == 1) {
-					if (arr[l] == arr[r]) isPalindrome[l][r] = EPalin::palind;
-					else isPalindrome[l][r] = EPalin::nonPalind;
-					break;
-				}
-				++l;
-				--r;
-			}
-			else break;
-		}
-		if (isPalindrome[l][r] == EPalin::palind) {
-			--l;
-			++r;
-			while (l >= from) {
-				if (arr[l] == arr[r]) {
-					isPalindrome[l--][r++] = EPalin::palind;
-				}
-				else break;
-			}
-		}
-		while (l >= from) {
-			isPalindrome[l--][r++] = EPalin::nonPalind;
-		}
-		if (isPalindrome[from][to] == EPalin::palind) std::cout << 1 << '\n';
-		else std::cout << 0 << '\n';
-	}
+    std::vector<int> palindromeLen(len);
+    int rightEnd = 1;
+    int palindromeCenter = 1;
+    for (int i = 1; i < len; ++i) {
+        int leftSide = 0;
+        if (rightEnd > i) {
+            if (i + palindromeLen[palindromeCenter - (i - palindromeCenter)] < rightEnd) {
+                palindromeLen[i] = palindromeLen[palindromeCenter - (rightEnd - palindromeCenter)];
+                continue;
+            }
+            else leftSide = i - (++rightEnd - i);
+        }
+        else {
+            leftSide = i - 1;
+            rightEnd = i + 1;
+        }
+        palindromeCenter = i;
+        while (leftSide >= 0 && rightEnd < len && manacherArr[leftSide] == manacherArr[rightEnd]) {
+            ++rightEnd;
+            --leftSide;
+        }
+        palindromeLen[i] = --rightEnd - i;
+    }
+    int queries;
+    std::cin >> queries;
+    for (int i = 0, from, to; i < queries; ++i) {
+        std::cin >> from >> to;
+        int target = ((from + to) >> 1) - 1;
+        int isOdd = (from + to) % 2;
+        int targetIdx = (target << 1) + 1 + isOdd;
+        if (target + (palindromeLen[targetIdx] / 2) >= to - 1) std::cout << 1 << '\n';
+        else std::cout << 0 << '\n';
+    }
 }
