c054596aa7296ac9ad4a1ab010aeebb1a4647a8f
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-01 22:36:23 +0900

    Update ReverseInteger.cpp

--- leetcode/ReverseInteger.cpp
+++ leetcode/ReverseInteger.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        long newVal = 0;
+        int newVal = 0;
+        int overLimit = numeric_limits<int>::max() / 10;
+        int underLimit = numeric_limits<int>::min() / 10;
         while (x) {
+            if (newVal > overLimit || newVal < underLimit) return 0;
             newVal *= 10;
             newVal += x % 10;
             x /= 10;
         }
-        if (newVal > numeric_limits<int>::max() || newVal < numeric_limits<int>::min()) return 0;
         return newVal;
     }
 };
