68e5bb63fd4a9d00a6f528ce2c069c9011020c1f
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-05 23:48:13 +0900

    Update RegularExpressionMatching.cpp

--- leetcode/RegularExpressionMatching.cpp
+++ leetcode/RegularExpressionMatching.cpp
@@ -1,45 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Solution {
-    bool checkStar(string& s, string& p, int pIdx, int sIdx, char preceding) {
-        while (pIdx < p.size()) {
-            if (sIdx == s.size()) {
-                if (p[pIdx] != '*' && (pIdx + 1 == p.size() || p[pIdx + 1] != '*')) {
-                    return false;
-                }
-                ++pIdx;
-                while (pIdx < p.size() && p[pIdx] == '*') ++pIdx;
-                continue;
-            }
-            if (p[pIdx] == '*') {
-                if (!preceding && pIdx) {
-                    preceding = p[pIdx - 1];
-                    while (pIdx + 1 < p.size() && p[pIdx + 1] == '*') ++pIdx;
-                }
-                else {
-                    if (preceding == '.' || preceding == s[sIdx]) {
-                        if (checkStar(s, p, pIdx, sIdx + 1, preceding)) return true;
-                    }
-                    ++pIdx;
-                    preceding = 0;
-                }
-            }
-            else if (p[pIdx] != s[sIdx] && p[pIdx] != '.') {
-                if (pIdx + 1 < p.size() && p[pIdx + 1] == '*') {
-                    pIdx += 2;
-                }
-                else return false;
-            }
-            else {
-                ++pIdx;
-                if (pIdx < p.size() && p[pIdx] == '*') {
-                    if (checkStar(s, p, pIdx + 1, sIdx, 0)) return true;
-                }
-                ++sIdx;
-            }
-        }
-        return sIdx == s.size();
-    }
 public:
     bool isMatch(string s, string p) {
-        return checkStar(s, p, 0, 0, 0);
+        for (int i = 0; i < p.size(); ++i) {
+            if (p[i] == '*') {
+                int til = i + 1;
+                for (; til < p.size(); ++til) {
+                    if (p[til] != '*') break;
+                }
+                --til;
+                p.erase(i + 1, til - i);
+            }
+        }
+        vector<vector<bool>> dp(s.size() + 1, vector<bool>(p.size() + 1));
+        dp[0][0] = true;
+        for (int i = 0; i < p.size(); ++i) {
+            if (p[i] == '*') {
+                if (i == 1) dp[0][i + 1] = true;
+                else dp[0][i + 1] = dp[0][i - 1];
+            }
+        }
+        for (int i = 1; i < dp.size(); ++i) {
+            for (int j = 1; j < dp[i].size(); ++j) {
+                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else if (p[j - 1] == '*') {
+                    if (j < 2) continue;
+                    dp[i][j] = dp[i][j - 2] || (p[j - 2] == '.' || (p[j - 2] == s[i - 1] && dp[i][j-1]));
+                }
+            }
+        }
+        return dp.back().back();
     }
 };
