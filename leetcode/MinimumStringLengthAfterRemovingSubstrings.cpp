00d99e8b3d7c4aa7d7a09688efc68512375725af
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-07 23:19:36 +0900

    Update MinimumStringLengthAfterRemovingSubstrings.cpp

--- leetcode/MinimumStringLengthAfterRemovingSubstrings.cpp
+++ leetcode/MinimumStringLengthAfterRemovingSubstrings.cpp
@@ -1,28 +1,13 @@
 class Solution {
-    int substrToRemove(string& s, int left, int right) {
-        while (left >= 0 && right < s.size()) {
-            if (s[left] != 'A') {
-                if (s[left] != 'C' || s[right] != 'D') break;
-            }
-            else if (s[right] != 'B') break;
-            --left; ++right;
-        }
-        s.erase(left + 1, right - left - 1);
-        if (left < 0) return 0;
-        return left;
-    }
 public:
     int minLength(string s) {
-        int idx = 0;
-        while (idx < s.size()) {
-            if (s[idx] == 'A' && idx + 1 < s.size() && s[idx + 1] == 'B') {
-                idx = substrToRemove(s, idx, idx + 1);
+        stack<char> stk;
+        for (int i = 0, end = s.size(); i < end; ++i) {
+            if (!stk.empty() && ((s[i] == 'B' && stk.top() == 'A') || (s[i] == 'D' && stk.top() == 'C'))) {
+               stk.pop();
             }
-            else if (s[idx] == 'C' && idx + 1 < s.size() && s[idx + 1] == 'D') {
-                idx = substrToRemove(s, idx, idx + 1);
-            }
-            else ++idx;
+            else stk.push(s[i]);
         }
-        return s.size();
+        return stk.size();
     }
 };
