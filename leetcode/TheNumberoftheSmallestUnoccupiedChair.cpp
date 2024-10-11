403b63172f62f839367271c640fce079bd132393
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-11 23:30:58 +0900

    Update TheNumberoftheSmallestUnoccupiedChair.cpp

--- leetcode/TheNumberoftheSmallestUnoccupiedChair.cpp
+++ leetcode/TheNumberoftheSmallestUnoccupiedChair.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
-        set<int> chairs;
+        priority_queue<int, vector<int>, greater<int>> chairs;
         int lastChair = 0;
         int targetEnter = times[targetFriend][0];
-        vector<vector<int>> leavings(targetEnter+1);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> leavings;
         sort(times.begin(), times.end(), [](vector<int>& one, vector<int>& two) {
             return one[0] < two[0];
         });
-        int timesChecked = 0;
         for (vector<int>& val : times) {
-            for (; timesChecked <= val[0]; ++timesChecked) {
-                for (int chairReturned : leavings[timesChecked]) {
-                    chairs.insert(chairReturned);
-                }
+            while (!leavings.empty() && leavings.top().first <= val[0]) {
+                chairs.push(leavings.top().second);
+                leavings.pop();
             }
             int chair = 0;
             if (chairs.empty()) {
                 chair = lastChair++;
             }
             else {
-                chair = *chairs.begin();
-                chairs.erase(chairs.begin());
+                chair = chairs.top();
+                chairs.pop();
             }
             if (val[0] == targetEnter) return chair;
             if (val[1] <= targetEnter) {
-                leavings[val[1]].push_back(chair);
+                leavings.push({val[1], chair});
             }
         }
         return 0;
