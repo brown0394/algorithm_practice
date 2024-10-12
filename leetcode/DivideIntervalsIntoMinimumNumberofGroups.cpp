c29aee02369d87908191ff25c51bad546eb2ee14
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-12 23:53:16 +0900

    Update DivideIntervalsIntoMinimumNumberofGroups.cpp

--- leetcode/DivideIntervalsIntoMinimumNumberofGroups.cpp
+++ leetcode/DivideIntervalsIntoMinimumNumberofGroups.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](vector<int>& one, vector<int>& two) {
-            if (one[0] == two[0]) return (one[1] - one[0] < two[1] - two[0]);
-            return one[0] < two[0];
-        });
-        map<int, int> groups;
-        int totGroups = 0;
-        for (int i = 0, len = intervals.size(); i < len; ++i) {
-            auto found = groups.lower_bound(intervals[i][0]);
-            if (found != groups.begin()) {
-                --found;
-                if (found->second > 1) --found->second;
-                else groups.erase(found);
-                --totGroups;
-            }
-            found = groups.lower_bound(intervals[i][1]);
-            if (found != groups.end() && found->first == intervals[i][1]) ++found->second;
-            else groups.emplace_hint(found, intervals[i][1], 1);
-            ++totGroups;
+        int lowest = numeric_limits<int>::max();
+        int highest = 0;
+        for (vector<int>& interval : intervals) {
+            if (interval[0] < lowest) lowest = interval[0];
+            if (interval[0] > highest) highest = interval[0];
         }
-        return totGroups;
+        vector<int> overlaps((highest - lowest) + 2);
+        highest = overlaps.size();
+        for (vector<int>& interval : intervals) {
+            ++overlaps[interval[0] - lowest];
+            if (interval[1] + 1 - lowest < highest) {
+                --overlaps[interval[1] + 1 - lowest];
+            }
+            
+        }
+        int minGroupsNeeded = 0;
+        int overlapCount = 0;
+        for (int val : overlaps) {
+            overlapCount += val;
+            if (overlapCount > minGroupsNeeded) minGroupsNeeded = overlapCount;
+        }
+        return minGroupsNeeded;
     }
 };
