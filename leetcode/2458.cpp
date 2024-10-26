94f596dad5c4496134e21d2f29af979c79234fdc
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-26 13:32:00 +0900

    Update 2458.cpp

--- leetcode/2458.cpp
+++ leetcode/2458.cpp
@@ -10,33 +10,31 @@
  * };
  */
 class Solution {
-    vector<vector<pair<int, int>>> levelNodes;
-    vector<int> levelIdx;
-    int dfs(TreeNode* cur, int level) {
-        if (levelNodes.size() <= level) levelNodes.resize(level + 1);
-        if (levelIdx.size() <= cur->val) levelIdx.resize(cur->val + 1);
-        levelIdx[cur->val] = level;
-        int deepest = level;
-        if (cur->left != nullptr) {
-            deepest = dfs(cur->left, level + 1);
-        }
-        if (cur->right != nullptr) {
-            int ret = dfs(cur->right, level + 1);
-            if (ret > deepest) deepest = ret;
-        }
-        levelNodes[level].push_back({cur->val, deepest});
-        return deepest;
+    int maxLevel;
+    int maxAfterPruning[100005];
+    void lfdfs(TreeNode* cur, int level) {
+        if (cur == nullptr) return;
+        maxAfterPruning[cur->val] = max(maxLevel, level - 1);
+        maxLevel = max(maxLevel, level);
+        lfdfs(cur->left, level + 1);
+        lfdfs(cur->right, level + 1);
+    }
+    void rfdfs(TreeNode* cur, int level) {
+        if (cur == nullptr) return;
+        maxAfterPruning[cur->val] = max(maxLevel, maxAfterPruning[cur->val]);
+        maxLevel = max(maxLevel, level);
+        rfdfs(cur->right, level + 1);
+        rfdfs(cur->left, level + 1);
     }
 public:
     vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
-        dfs(root, 0);
+        maxLevel = 0;
+        lfdfs(root, 0);
+        maxLevel = 0;
+        rfdfs(root, 0);
         vector<int> ans(queries.size());
         for (int i = 0, len = queries.size(); i < len; ++i) {
-            int maxVal = levelIdx[queries[i]] - 1;
-            for (pair<int, int>& val : levelNodes[levelIdx[queries[i]]]) {
-                if (val.first != queries[i] && val.second > maxVal) maxVal = val.second;
-            }
-            ans[i] = maxVal;
+            ans[i] = maxAfterPruning[queries[i]];
         }
         return ans;
     }
