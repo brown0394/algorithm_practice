9835be2339b7ba3dd19b5bc91df567bd1c55184a
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-29 22:53:49 +0900

    Update 2684.cpp

--- leetcode/2684.cpp
+++ leetcode/2684.cpp
@@ -4,31 +4,25 @@ public:
         int row = grid.size();
         int col = grid[0].size();
         int maxMoves = 0;
-        vector<vector<int>> moveCounts(row, vector<int>(col));
-        queue<pair<int , int>> q;
-        for (int i = 0; i < row; ++i) q.push({i, 0});
-        while (!q.empty()) {
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-            if (i && grid[i][j] < grid[i - 1][j + 1] && 
-            moveCounts[i][j] >= moveCounts[i - 1][j + 1]) {
-                if (moveCounts[i - 1][j + 1] == 0 && j + 2 < col) q.push({i - 1, j + 1});
-                moveCounts[i - 1][j + 1] = moveCounts[i][j] + 1;
-                maxMoves = max(maxMoves, moveCounts[i - 1][j + 1]);
+        vector<vector<int>> moveCounts(row, vector<int>(2, 1));
+        int last = 0, cur = 1;
+        for (int j = 1; j < col; ++j) {
+            for (int i = 0; i < row; ++i) {
+                moveCounts[i][cur] = 0;
+                if (i && grid[i - 1][j - 1] < grid[i][j] && moveCounts[i-1][last]) {
+                    moveCounts[i][cur] = max(moveCounts[i][cur], moveCounts[i-1][last] + 1);
+                }
+                if (grid[i][j - 1] < grid[i][j] && moveCounts[i][last]) {
+                    moveCounts[i][cur] = max(moveCounts[i][cur], moveCounts[i][last] + 1);
+                }
+                if (i + 1 < row && grid[i + 1][j - 1] < grid[i][j] && moveCounts[i+1][last]) {
+                    moveCounts[i][cur] = max(moveCounts[i][cur], moveCounts[i+1][last] + 1);
+                }
+                maxMoves = max(maxMoves, moveCounts[i][cur]);
             }
-            if (grid[i][j] < grid[i][j + 1] && moveCounts[i][j] >= moveCounts[i][j + 1]) {
-                if (moveCounts[i][j + 1] == 0 && j + 2 < col) q.push({i, j + 1});
-                moveCounts[i][j + 1] = moveCounts[i][j] + 1;
-                maxMoves = max(maxMoves, moveCounts[i][j + 1]);
-            }
-            if (i + 1 < row && grid[i][j] < grid[i + 1][j + 1] && 
-            moveCounts[i][j] >= moveCounts[i + 1][j + 1]) {
-                if (moveCounts[i + 1][j + 1] == 0 && j + 2 < col) q.push({i + 1, j + 1});
-                moveCounts[i + 1][j + 1] = moveCounts[i][j] + 1;
-                maxMoves = max(maxMoves, moveCounts[i + 1][j + 1]);
-            }       
+            swap(last, cur);
         }
+        if (maxMoves) return maxMoves - 1;
         return maxMoves;
     }
 };
