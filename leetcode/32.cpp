1dc6c47d922fde3333f27497d4ded582f6257929
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-10-31 23:00:43 +0900

    Update 32.cpp

--- leetcode/32.cpp
+++ leetcode/32.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        vector<bool> valid(s.size());
         stack<int> stk;
+        int longest = 0;
+        stk.push(-1);
         for (int i = 0, len = s.size(); i < len; ++i) {
             if (s[i] == '(') {
                 stk.push(i);
             }
             else {
+                stk.pop();
                 if (!stk.empty()) {
-                    valid[stk.top()] = true;
-                    stk.pop();
-                    valid[i] = true;
+                    longest = max(longest, i - stk.top());
+                }
+                else {
+                    stk.push(i);
                 }
             }
         }
-        int longest = 0;
-        int count = 0;
-        for (int i = 0, len = valid.size(); i < len; ++i) {
-            if (valid[i]) ++count;
-            else {
-                longest = max(longest, count);
-                count = 0;
-            }
-        }
-        longest = max(longest, count);
         return longest;
     }
 };
