a5e2c0088fa3f33d9b1af117e245aad802dc02bc
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-01 23:09:49 +0900

    Update 41.cpp

--- leetcode/41.cpp
+++ leetcode/41.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int len = nums.size();
-        bool one = false;
         for (int i = 0; i < len; ++i) {
-            if (nums[i] == 1) one = true; 
-            if (nums[i] <= 0 || nums[i] > len) nums[i] = 1;
-        }
-        if (!one) return 1;
-        for (int i = 0; i < len; ++i) {
-            nums[abs(nums[i]) - 1] = -abs(nums[abs(nums[i]) - 1]);
+            while (nums[i] > 0 && nums[i] <= len && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i]-1]);
+            }
         }
         for (int i = 0; i < len; ++i) {
-            if (nums[i] > 0) return i + 1;
+            if (nums[i] != i + 1) return i + 1;
         }
         return len + 1;
     }
