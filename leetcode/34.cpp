1391b480682c01745b392e8fbb379b7772502c7e
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-01 12:45:01 +0900

    Create 34.cpp

---
+++ leetcode/34.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto l = lower_bound(nums.begin(), nums.end(), target);
+        auto r = upper_bound(nums.begin(), nums.end(), target);
+        
+        if (l == nums.end() || *l != target) return {-1, -1};
+        return {static_cast<int>(l - nums.begin()), static_cast<int>(r - nums.begin() - 1)};
+    }
+};
