e00df75c7339ca1ad0904dcbfdf1d04b1fcd03f7
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-08 23:14:04 +0900

    Update 45.cpp

--- leetcode/45.cpp
+++ leetcode/45.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int len = nums.size();
-        vector<int> jumpsNeeded(len, len);
-        jumpsNeeded[len - 1] = 0;
-        for (int i = len - 2; i >= 0; --i) {
-            jumpsNeeded[i] = len;
-            for (int j = 1; i + j < len && j <= nums[i]; ++j) {
-                jumpsNeeded[i] = min(jumpsNeeded[i], jumpsNeeded[i + j] + 1);
+        int len = nums.size() - 1;
+        if (len == 0) return 0;
+        int cur = 0, next = 0, nextPossible = nums[0];
+        int jumps = 1;
+        while (nextPossible < len) {
+            for (int i = 1; i <= nums[cur]; ++i) {
+                if (cur + i + nums[cur + i] > nextPossible) {
+                    nextPossible = cur + i + nums[cur + i];
+                    next = i;
+                }
             }
+            cur += next;
+            next = 0;
+            ++jumps; 
         }
-        return jumpsNeeded[0];
+        return jumps;
     }
 };
