2b6d669a0c208913ba0083be6fb7f7c509b5b294
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-11 22:55:54 +0900

    Update 2601.cpp

--- leetcode/2601.cpp
+++ leetcode/2601.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     bool primeSubOperation(vector<int>& nums) {
-        vector<int> primes;
         bitset<1001> TwoToThousand;
-        TwoToThousand[0] = true;
         TwoToThousand[1] = true;
         for (int i = 2; i <= 1000; ++i) {
             if (TwoToThousand[i]) continue;
-            primes.push_back(i);
             for (int j = i + i; j <= 1000; j += i) {
                 TwoToThousand[j] = true;
             }
         }
-        
-        for (int i = nums.size()-2; i >= 0; --i) {
-            if (nums[i] >= nums[i + 1]) {
-                auto found = lower_bound(primes.begin(), primes.end(), (nums[i] - nums[i+1]) + 1);
-                if (*found >= nums[i]) return false;
-                nums[i] -= *found;
-                if (nums[i] >= nums[i + 1]) return false;
+        int cur = 1;
+        for (int i = 0, size = nums.size(); i < size; ++i) {
+            while (true) {
+                int diff = nums[i] - cur;
+                ++cur;
+                if (diff < 0) return false;
+                if (!TwoToThousand[diff]) break;
             }
         }
         return true;
