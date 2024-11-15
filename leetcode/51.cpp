7692f42abbc9111b04d0489542b57f57497e53ca
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-15 23:00:27 +0900

    Update 51.cpp

--- leetcode/51.cpp
+++ leetcode/51.cpp
@@ -11,21 +11,19 @@ class Solution {
             if (j + plus < nn) placeable[i + plus][j + plus] += delta;
         }
     }
-    void putQueen(int i, int j, int totQueens) {
+    void putQueen(int i, int totQueens) {
         if (totQueens == nn) {
             ans.push_back(board);
         }
-        for (; i < nn; ++i) {
-            for (; j < nn; ++j) {
-                if (0 == placeable[i][j]) {
-                    board[i][j] = 'Q';
-                    markQ(i, j, 1);
-                    putQueen(i + 1, 0, totQueens + 1);
-                    board[i][j] = '.';
-                    markQ(i, j, -1);                   
-                }
+        if (i == nn) return;
+        for (int j = 0; j < nn; ++j) {
+            if (0 == placeable[i][j]) {
+                board[i][j] = 'Q';
+                markQ(i, j, 1);
+                putQueen(i + 1, totQueens + 1);
+                board[i][j] = '.';
+                markQ(i, j, -1);    
             }
-            j = 0;
         }
     }
 public:
@@ -33,7 +31,7 @@ public:
         nn = n;
         placeable.resize(n, vector<int>(n));
         board.resize(n, string(n, '.'));
-        putQueen(0, 0, 0);
+        putQueen(0, 0);
         return ans;
     }
 };
