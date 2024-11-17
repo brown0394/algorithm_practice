f9733aca927656da1d65e60dd83e8d1087a9c18e
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-18 01:46:26 +0900

    Update 862.cpp

--- leetcode/862.cpp
+++ leetcode/862.cpp
@@ -1,24 +1,38 @@
 class Solution {
+    int getIdx(long long val, deque<pair<long long, int>>& dq) {
+        int right = dq.size()-1;
+        int left = 0;
+        int idx = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (dq[mid].first > val) {
+                right = mid - 1;
+            }
+            else {
+                idx = mid;
+                left = mid + 1;
+            }
+        }
+        return idx;
+    }
 public:
     int shortestSubarray(vector<int>& nums, int k) {
         const int INV = 1000000;
         int shortestLen = INV;
         int size = nums.size();
-        if (size == 1) {
-            if (nums[0] >= k) return 1;
-            else return -1;
-        }
-        long long cur = nums[0];
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-        pq.push({nums[0], 0});
-        for (int i = 1; i < size; ++i) {
+        long long cur = 0;
+        deque<pair<long long, int>> dq;
+        dq.emplace_back(0, -1);
+        for (int i = 0; i < size; ++i) {
             cur += nums[i];
-            if (cur >= k) shortestLen = min(shortestLen, i + 1);
-            while (!pq.empty() && cur - pq.top().first >= k) {
-                shortestLen = min(shortestLen, i - pq.top().second);
-                pq.pop();
+            
+            while (!dq.empty() && dq.back().first >= cur) {
+                dq.pop_back();
             }
-            pq.push({cur, i});
+            int idx = getIdx(cur - k, dq);
+            if (idx >= 0) shortestLen = min(shortestLen, i - dq[idx].second);
+            dq.emplace_back(cur, i);
+            
         }
         if (shortestLen == INV) return -1;
         return shortestLen;
