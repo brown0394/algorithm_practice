4d19404db58bf344707da408c6afa99b3568ebd6
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-18 15:15:13 +0900

    Update 862.cpp

--- leetcode/862.cpp
+++ leetcode/862.cpp
@@ -1,38 +1,22 @@
 class Solution {
-    int getIdx(long long val, deque<pair<long long, int>>& dq) {
-        int right = dq.size()-1;
-        int left = 0;
-        int idx = -1;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-            if (dq[mid].first > val) {
-                right = mid - 1;
-            }
-            else {
-                idx = mid;
-                left = mid + 1;
-            }
-        }
-        return idx;
-    }
 public:
     int shortestSubarray(vector<int>& nums, int k) {
         const int INV = 1000000;
         int shortestLen = INV;
         int size = nums.size();
-        long long cur = 0;
-        deque<pair<long long, int>> dq;
-        dq.emplace_back(0, -1);
-        for (int i = 0; i < size; ++i) {
-            cur += nums[i];
-            
-            while (!dq.empty() && dq.back().first >= cur) {
-                dq.pop_back();
+        vector<long long> prefixSums(size + 1);
+        for (int i = 1; i <= size; ++i) {
+            prefixSums[i] = prefixSums[i-1] + nums[i-1];
+        }
+        deque<int> dq;
+        dq.push_back(0);
+        for (int i = 1; i <= size; ++i) {
+            while (!dq.empty() && prefixSums[i] - prefixSums[dq.front()] >= k) {
+                shortestLen = min(shortestLen, i - dq.front());
+                dq.pop_front();
             }
-            int idx = getIdx(cur - k, dq);
-            if (idx >= 0) shortestLen = min(shortestLen, i - dq[idx].second);
-            dq.emplace_back(cur, i);
-            
+            while (!dq.empty() && prefixSums[dq.back()] >= prefixSums[i]) dq.pop_back();
+            dq.emplace_back(i);
         }
         if (shortestLen == INV) return -1;
         return shortestLen;
