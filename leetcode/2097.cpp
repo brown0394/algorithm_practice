28e25fdb89efa9237576b11264107179e13244f3
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-11-30 23:56:35 +0900

    Update 2097.cpp

--- leetcode/2097.cpp
+++ leetcode/2097.cpp
@@ -1,31 +1,35 @@
 class Solution {
-    unordered_map<int, deque<int>> pairmap;
-    vector<int> visitSeq;
-    void visit(int n) {
-        while (!pairmap[n].empty()) {
-            int next = pairmap[n].front();
-            pairmap[n].pop_front();
-            visit(next);
-        }
-        visitSeq.push_back(n);
-    }
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
-        unordered_map<int, int> inbounds;
+        unordered_map<int, int> bounds;
+        unordered_map<int, deque<int>> pairmap;
+        vector<int> visitSeq;
         int numOfPairs = pairs.size();
         for (int i = 0; i < numOfPairs; ++i) {
             pairmap[pairs[i][0]].push_back(pairs[i][1]);
-            ++inbounds[pairs[i][1]];
+            ++bounds[pairs[i][0]];
+            --bounds[pairs[i][1]];
         }
 
         int start = pairmap.begin()->first;
-        for (auto& p : pairmap) {
-            if (inbounds[p.first] < p.second.size()) {
+        for (auto& p : bounds) {
+            if (p.second == 1) {
                 start = p.first;
                 break;
             }
         }
-        visit(start);
+        stack<int> stk;
+        stk.push(start);
+        while (!stk.empty()) {
+            start = stk.top();
+            if (pairmap[start].empty()) {
+                visitSeq.push_back(start);
+                stk.pop();
+                continue;
+            }
+            stk.push(pairmap[start].back());
+            pairmap[start].pop_back();
+        }
         vector<vector<int>> answer;
         for (int i = visitSeq.size()-2; i >= 0; --i) {
             answer.push_back({visitSeq[i+1], visitSeq[i]});
