75e79d264a60c6c65fc79b8f89a5d2cb292c86b1
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-02 22:57:17 +0900

    Update 57.cpp

--- leetcode/57.cpp
+++ leetcode/57.cpp
@@ -4,32 +4,16 @@ public:
         int len = intervals.size();
         if (!len) return {newInterval};
         vector<vector<int>> answer;
-        if (intervals[0][0] > newInterval[1]) {
-            answer.push_back(newInterval);
-            answer.insert(answer.end(), intervals.begin(), intervals.end());
-            return answer;
-        }
-        if (intervals.back()[1] < newInterval[0]) {
-            answer = intervals;
-            answer.push_back(newInterval);
-            return answer;
-        }
         int i = 0;
-        for (; i < len; ++i) {
-            if (intervals[i][1] < newInterval[0]) {
-                answer.push_back(intervals[i]);
-                continue;
-            }
-            if (intervals[i][0] > newInterval[1]) {
-                answer.push_back(newInterval);
-                break;
-            }
+        for (; i < len && intervals[i][1] < newInterval[0]; ++i) {
+            answer.push_back(intervals[i]);
+        }
+        if (i < len && newInterval[1] >= intervals[i][0]) {
             int smaller = min(intervals[i][0], newInterval[0]);
             int bigger = max(intervals[i][1], newInterval[1]);
             answer.push_back({smaller, bigger});
-            ++i;
-            break;
         }
+        else answer.push_back(newInterval);
         for (; i < len && answer.back()[1] >= intervals[i][0]; ++i) {
             answer.back()[1] = max(answer.back()[1], intervals[i][1]);
         }
