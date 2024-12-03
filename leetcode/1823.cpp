3085d150d6863348a73a7ed1bc0c40f6c59ca1c0
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-03 16:01:43 +0900

    Update 1823.cpp

--- leetcode/1823.cpp
+++ leetcode/1823.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        list<int> circle;
-        for (int i = 1; i <= n; ++i) circle.push_back(i);
-        auto it = circle.begin();
-        while (n > 1) {
-            int count = 1;
-            while(count < k) {
-                ++it;
-                if (it == circle.end()) it = circle.begin();
-                ++count;
-            }
-            it = circle.erase(it);
-            if (it == circle.end()) it = circle.begin();
-            --n;
+        int carry = 0;
+        for (int i = 2; i <= n; ++i) {
+            carry = (carry + k) % i;
         }
-        return *it;
+        return carry + 1;
     }
 };
