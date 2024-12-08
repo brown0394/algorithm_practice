b0fa3ee397bb25af594adca5c99f3fbf15eb25c4
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-09 00:01:00 +0900

    Update 2054.cpp

--- leetcode/2054.cpp
+++ leetcode/2054.cpp
@@ -1,31 +1,21 @@
 class Solution {
-    vector<vector<int>> memo;
-    int size;
     static bool compare(int value, vector<int>& two) {
         return value < two[0];
     }
-    int check(vector<vector<int>>& events, int idx) {
-        int next = upper_bound(events.begin()+idx, events.end(), events[idx][1], compare) - events.begin();
-        if (next == size) {
-            return events[idx][2];
-        }
-        int maxV = 0;
-        for (; next < size; ++next) {
-            if (memo[idx][next] == -1) {
-                memo[idx][next] = check(events, next) + events[idx][2];
-            }
-            maxV = max(maxV, memo[idx][next]);
-        }
-        return maxV;
-    }
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        size = events.size();
-        memo.resize(size, vector<int>(size, -1));
+        int size = events.size();
         sort(events.begin(), events.end());
+        vector<int> maxVs(size);
+        maxVs[size-1] = events[size-1][2];
+        for (int i = size-2; i > 0; --i) {
+            maxVs[i] = max(maxVs[i+1], events[i][2]);
+        }
         int ans = 0;
         for (int i = 0; i < size; ++i) {
-            ans = max(ans, check(events, i));
+            int next = upper_bound(events.begin()+i+1, events.end(), events[i][1], compare) - events.begin();
+            if (next == size) ans = max(ans, events[i][2]);
+            else ans = max(ans, events[i][2] + maxVs[next]);
         }
         return ans;
     }
