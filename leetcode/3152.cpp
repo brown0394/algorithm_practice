bb855c067e4c3777d17ef183fba5945e62bf50c0
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-10 00:07:51 +0900

    Update 3152.cpp

--- leetcode/3152.cpp
+++ leetcode/3152.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int size = nums.size();
-        vector<int> falseIdx;
-        for (int i = 0; i < size - 1; ++i) {
-            if (nums[i] % 2 == nums[i+1] % 2) falseIdx.push_back(i);
+        vector<int> falsePrefix(size);
+        for (int i = 1; i < size; ++i) {
+            falsePrefix[i] = falsePrefix[i-1];
+            if (nums[i] % 2 == nums[i-1] % 2) ++falsePrefix[i];
         }
         size = queries.size();
         vector<bool> ans(size);
         for (int i = 0; i < size; ++i) {
-            auto it = lower_bound(falseIdx.begin(), falseIdx.end(), queries[i][0]);
-            if (it == falseIdx.end()) {
-                ans[i] = true;
-            }
-            else if (*it + 1 <= queries[i][1]) ans[i] = false;
-            else ans[i] = true;
+            if (falsePrefix[queries[i][0]] == falsePrefix[queries[i][1]]) ans[i] = true;
+            else ans[i] = false;
         }
         return ans;
     }
