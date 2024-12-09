8e0c5fa82dfe59374bf1dd2bcd00eaa96a04ee8a
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-09 23:48:20 +0900

    Update 3152.cpp

--- leetcode/3152.cpp
+++ leetcode/3152.cpp
@@ -7,26 +7,14 @@ public:
             if (nums[i] % 2 == nums[i+1] % 2) falseIdx.push_back(i);
         }
         size = queries.size();
-        vector<vector<int>> qs;
-        qs.reserve(size);
-        for (int i = 0; i < size; ++i) {
-            qs.push_back(queries[i]);
-            qs[i].push_back(i);
-        }
-        sort(qs.begin(), qs.end());
-        int idx = 0;
-        int fsize = falseIdx.size();
         vector<bool> ans(size);
-        
-        for (int a : falseIdx) cout << a << endl;
         for (int i = 0; i < size; ++i) {
-            while (idx < fsize && falseIdx[idx] < qs[i][0]) ++idx;
-            if (idx == fsize) {
-                ans[qs[i][2]] = true;
-                continue;
+            auto it = lower_bound(falseIdx.begin(), falseIdx.end(), queries[i][0]);
+            if (it == falseIdx.end()) {
+                ans[i] = true;
             }
-            if (falseIdx[idx] + 1 <= qs[i][1]) ans[qs[i][2]] = false;
-            else ans[qs[i][2]] = true;
+            else if (*it + 1 <= queries[i][1]) ans[i] = false;
+            else ans[i] = true;
         }
         return ans;
     }
