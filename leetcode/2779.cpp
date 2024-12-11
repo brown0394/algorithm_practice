41b0bd85fbbb0ddbe694f77ebf22e89eb2a1e363
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-11 23:39:28 +0900

    Update 2779.cpp

--- leetcode/2779.cpp
+++ leetcode/2779.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        int count[300005]{0};
+        sort(nums.begin(), nums.end());
+        auto comp = [k](int v, int v2) {
+            return v < v2 - k;
+        };
         int maxV = 0;
-        for (int i = 0, len = nums.size(); i < len; ++i) {
-            ++count[nums[i] + 100000];
-            maxV = max(maxV, count[nums[i] + 100000]);
-            for (int j = 1; j <= k; ++j) {
-                ++count[nums[i] + 100000 + j];
-                maxV = max(maxV, count[nums[i] + 100000 + j]);
-                ++count[nums[i] + 100000 - j];
-                maxV = max(maxV, count[nums[i] + 100000 - j]);
+        int n = nums.size();
+        for (auto it = nums.begin(), end = nums.end(); it != end; ++it) {
+            auto found = upper_bound(it, end, *it + k, comp);
+            int count = 0;
+            if (found == end) {
+                count = n - (it - nums.begin());
             }
+            else count = found - it;
+            maxV = max(maxV, count);
         }
         return maxV;
     }
