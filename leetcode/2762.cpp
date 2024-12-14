0533145d49030b939c9ed79c63338c81a417442b
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-14 23:44:06 +0900

    Update 2762.cpp

--- leetcode/2762.cpp
+++ leetcode/2762.cpp
@@ -3,29 +3,24 @@ public:
     long long continuousSubarrays(vector<int>& nums) {
         int size = nums.size();
         long long total = 0;
-        int left = 0, right = 1;
-        int minV = nums[0];
-        int maxV = minV;
-        bool minus = false;
-        while (true) {
-            while(right < size && abs(nums[right] - minV) <= 2 &&
-             abs(nums[right] - maxV) <= 2) {
-                minV = min(minV, nums[right]);
-                maxV = max(maxV, nums[right]);
-                ++right;
-             }
-            long long N = right - left;
-            total += ((N * (N + 1)) >> 1);
-            if (minus) --total;
-            if (right == size) break;
-            if (right - left == 1) {
-                ++right;
-                minus = false;
+        int left = 0, right = 0;
+        map<int, int> counts;
+        while (right < size) {
+            auto found = counts.find(nums[right]);
+            if (found == counts.end()) counts.insert({nums[right], 1});
+            else ++found->second;
+            while (!counts.empty() && abs(counts.begin()->first - counts.rbegin()->first) > 2)
+            {
+                found = counts.find(nums[left]);
+                if (found->second > 1) --found->second;
+                else counts.erase(found);
+                ++left;
             }
-            else minus = true;
-            left = right - 1;
-            minV = nums[left];
-            maxV = nums[left];
+            if (left <= right) {
+                long long n = right - left + 1;
+                total += n;
+            }
+            ++right;
         }
         return total;
     }
