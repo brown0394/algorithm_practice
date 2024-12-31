3a34e13f99beb2a4555475fbff28db5a237d11c4
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-12-31 22:02:05 +0900

    Update 983.cpp

--- leetcode/983.cpp
+++ leetcode/983.cpp
@@ -1,28 +1,24 @@
 class Solution {
-    int size;
-    vector<int> memo;
-    int includedDays[2];
-    void plan(vector<int>& days, vector<int>& costs, int idx, int cost) {
-        if (memo[idx] < cost) return;
-        memo[idx] = cost;
-        if (idx == size) return;
-        
-        for (int i = 0; i < 2; ++i) {
-            auto found = lower_bound(days.begin()+idx, days.end(), days[idx] + includedDays[i]);
-            int next = size;
-            if (found != days.end()) next = found - days.begin();
-            plan(days, costs, next, cost + costs[2-i]);
-        }
-        plan(days, costs, idx+1, cost + costs[0]);
-    }
+
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int cur = 0;
-        size = days.size();
-        memo.resize(size+1, 1000000000);
-        includedDays[0] = 30;
-        includedDays[1] = 7;
-        plan(days, costs, 0, 0);
+        int size = days.size();
+        vector<int> memo(size+1, 1000000000);
+        memo[0] = 0;
+        auto found = lower_bound(days.begin(), days.end(), days[0] + 7);
+        int weekLater = size;
+        if (found != days.end()) weekLater = found - days.begin();
+        found = lower_bound(days.begin(), days.end(), days[0] + 30);
+        int monthLater = size;
+        if (found != days.end()) monthLater = found - days.begin();
+        for (int i = 0; i < size; ++i) {
+            memo[i+1] = min(memo[i+1], memo[i] + costs[0]);
+            while(weekLater < size && days[weekLater] - days[i] < 7) ++weekLater;
+            while(monthLater < size && days[monthLater] - days[i] < 30) ++monthLater;
+            memo[weekLater] = min(memo[weekLater], memo[i] + costs[1]);
+            memo[monthLater] = min(memo[monthLater], memo[i] + costs[2]);
+        }
         return memo[size];
     }
 };
