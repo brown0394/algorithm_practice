a4c76b479ee4386d63bfed1fc5e5b9eaf13f8b81
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2025-01-10 22:28:01 +0900

    Update 916.cpp

--- leetcode/916.cpp
+++ leetcode/916.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        vector<unordered_map<char, int>> alphaCounts(words1.size());
-        unordered_map<char, int> alphaCounts2;
-        for (int i = 0; i < words1.size(); ++i)
+        vector<array<int, 26>> alphaCounts(words1.size());
+        array<int, 26> alphaCounts2 {};
+        for (int i = 0, len = words2.size(); i < len; ++i)
         {
-            for (char c : words1[i])
+            array<int, 26> tempCounts {};
+            for (int j = 0, len2 = words2[i].size(); j < len2; ++j)
             {
-                ++alphaCounts[i][c];
+                ++tempCounts[words2[i][j]-'a'];
             }
-        }
-        for (int i = 0; i < words2.size(); ++i)
-        {
-            unordered_map<char, int> tempCounts;
-            for (char c : words2[i])
+            for (int j = 0; j < 26; ++j)
             {
-                ++tempCounts[c];
-            }
-            for (pair<const char, int>& p : tempCounts)
-            {
-                alphaCounts2[p.first] = max(alphaCounts2[p.first], p.second);
+                alphaCounts2[j] = max(alphaCounts2[j], tempCounts[j]);
             }
         }
         vector<string> ans;
-        for (int i = 0; i < alphaCounts.size(); ++i) 
+        for (int i = 0; i < words1.size(); ++i) 
         {
-            bool isUniversal = true;
-            for (pair<const char, int>& p : alphaCounts2)
+            for (int j = 0, len2 = words1[i].size(); j < len2; ++j)
             {
-                if (alphaCounts[i][p.first] < p.second)
+                ++alphaCounts[i][words1[i][j]-'a'];
+            }
+            bool isUniversal = true;
+            for (int j = 0; j < 26; ++j)
+            {
+                if (alphaCounts[i][j] < alphaCounts2[j])
                 {
                     isUniversal = false;
                     break;
