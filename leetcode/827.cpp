0ad71d4510f5abd78d9a62d4d86dbc0fde683cff
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2025-01-31 18:06:26 +0900

    Update 827.cpp

--- leetcode/827.cpp
+++ leetcode/827.cpp
@@ -2,54 +2,46 @@ class Solution {
 public:
     int largestIsland(vector<vector<int>>& grid) {
         int row = grid.size(), col = grid[0].size();
-        vector<vector<bool>> visited(row, vector<bool>(col));
-        vector<vector<pair<int,int>>> countGrid(row, vector<pair<int, int>>(col));
-        int id = 1;
+        unordered_map<int, int> idMap;
+        int id = 2;
+        idMap[0] = 0;
         for (int i = 0; i < row; ++i)
         {
             for (int j = 0; j < col; ++j)
             {
-                if (grid[i][j] > 0 && visited[i][j] == false)
+                if (grid[i][j] == 1)
                 {
                     queue<pair<int, int>> q;
-                    stack<pair<int, int>> stk;
-                    visited[i][j] = true;
                     q.push({i, j});
+                    int count = 0;
+                    grid[i][j] = id;
                     while (q.empty() == false)
                     {
                         auto [r, c] = q.front();
                         q.pop();
-                        stk.push({r, c});
-                        if (r + 1 < row && visited[r+1][c] == false && grid[r+1][c] > 0)
+                        ++count;
+                        if (r + 1 < row && grid[r+1][c] == 1)
                         {
-                            visited[r+1][c] = true;
+                            grid[r+1][c] = id;
                             q.push({r+1, c});
                         }
-                        if (r > 0 && visited[r-1][c] == false && grid[r-1][c] > 0)
+                        if (r > 0 && grid[r-1][c] == 1)
                         {
-                            visited[r-1][c] = true;
                             q.push({r-1, c});
+                            grid[r-1][c] = id;
                         }
-                        if (c + 1 < col && visited[r][c+1] == false && grid[r][c+1] > 0)
+                        if (c + 1 < col && grid[r][c+1] == 1)
                         {
-                            visited[r][c+1] = true;
                             q.push({r, c+1});
+                            grid[r][c+1] = id;
                         }
-                        if (c > 0 && visited[r][c-1] == false && grid[r][c-1] > 0)
+                        if (c > 0 && grid[r][c-1] == 1)
                         {
-                            visited[r][c-1] = true;
                             q.push({r, c-1});
+                            grid[r][c-1] = id;
                         }
                     }
-                    int size = stk.size();
-                    while (stk.empty() == false)
-                    {
-                        auto [r, c] = stk.top();
-                        stk.pop();
-                        countGrid[r][c].first = size;
-                        countGrid[r][c].second = id;
-                    }
-                    ++id;
+                    idMap[id++] = count;
                 }
             }
         }
@@ -58,36 +50,34 @@ public:
         {
             for (int j = 0; j < col; ++j)
             {
-                if (countGrid[i][j].first > 0)
+                if (grid[i][j] > 0)
                     continue;
                 int sum = 1;
-                pair<int, int> l, r, u, d;
-                if (j > 0) l = countGrid[i][j-1];
-                if (j+1 < col) r = countGrid[i][j+1];
-                if (i > 0) u = countGrid[i-1][j];
-                if (i+1 < row) d = countGrid[i+1][j];
                 unordered_set<int> check;
-                check.insert(l.second);
-                sum += l.first;
-                if (check.find(r.second) == check.end())
+                if (j > 0) 
                 {
-                    sum += r.first;
-                    check.insert(r.second);
+                    check.insert(grid[i][j-1]);
+                    sum += idMap[grid[i][j-1]];
                 }
-                if (check.find(u.second) == check.end())
+                if (j+1 < col && check.find(grid[i][j+1]) == check.end())
                 {
-                    sum += u.first;
-                    check.insert(u.second);
+                    sum += idMap[grid[i][j+1]];
+                    check.insert(grid[i][j+1]);
                 }
-                if (check.find(d.second) == check.end())
+                if (i > 0 && check.find(grid[i-1][j]) == check.end())
                 {
-                    sum += d.first;
-                    check.insert(d.second);
+                    sum += idMap[grid[i-1][j]];
+                    check.insert(grid[i-1][j]);
+                }
+                if (i+1 < row && check.find(grid[i+1][j]) == check.end())
+                {
+                    sum += idMap[grid[i+1][j]];
+                    check.insert(grid[i+1][j]);
                 }
                 ans = max(ans, sum);
             }
         }
-        if (ans == 0) return countGrid[0][0].first;
+        if (ans == 0) return idMap[grid[0][0]];
         return ans;
     }
 };
